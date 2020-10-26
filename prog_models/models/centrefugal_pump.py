0cbfe1480b3702020d612a1f6320f77ef8a8b5a2
Christopher Teubert <christopher.a.teubert@nasa.gov>
2020-10-26 09:52:27 -0700

    Completed first draft

--- prog_models/models/centrefugal_pump.py
+++ prog_models/models/centrefugal_pump.py
@@ -1,8 +1,8 @@
-from .. import deriv_prog_model
+from .. import prognostics_model
 
 import math
 
-class CentrefugalPump(deriv_prog_model.DerivProgModel):
+class CentrefugalPump(prognostics_model.PrognosticsModel):
     """
     Prognostics model for a centrefugal pump
     """
@@ -10,7 +10,7 @@ class CentrefugalPump(deriv_prog_model.DerivProgModel):
         'ImpellerWearFailure',
         'PumpOilOverheat',
         'RadialBearingOverheat',
-        'ImpellerWearFailure'
+        'ThrustBearingOverheat'
     ]
     
     inputs = [
@@ -30,19 +30,21 @@ class CentrefugalPump(deriv_prog_model.DerivProgModel):
         'rRadial',
         'rThrust',
         'w',
+        'wA',
         'wRadial',
-        'wThrust'
+        'wThrust',
+        'QLeak'
     ]
 
     outputs = [
-        'Qoutm',# Discharge Flow (m^3/s)
-        'Tom',  # Oil Temperature (K)
-        'Trm',  # Radial Bearing Temperature (K)
-        'Ttm',  # Thrust Bearing Temperature (K)
-        'wm'    # Mechanical Rotation (rad/sec)
+        'Qout',# Discharge Flow (m^3/s)
+        'To',  # Oil Temperature (K)
+        'Tr',  # Radial Bearing Temperature (K)
+        'Tt',  # Thrust Bearing Temperature (K)
+        'w'    # Mechanical Rotation (rad/sec)
     ]
 
-    parameters { # Set to defaults
+    parameters = { # Set to defaults
         'cycleTime': 3600,  # length of a pump usage cycle
 
         # Environmental parameters
@@ -108,19 +110,73 @@ class CentrefugalPump(deriv_prog_model.DerivProgModel):
             'wThrust': 0,
             'wRadial': 0
         }
-
-        def initialize(self, u, z):
-            return self.parameters['x0']
-
-        def dx(self, t, x, u):
-            pass
-
-        def output(self, t, x):
-            pass
-
-        def event_state(self, t, x):
-            pass
-
-        def threshold_met(self, t, x):
-            pass
     }
+
+    def initialize(self, u, z):
+        x0 = self.parameters['x0']
+        x0['QLeak'] = math.copysign(self.parameters['cLeak']*self.parameters['ALeak']*math.sqrt(abs(u['psuc']-u['pdisch'])), u['psuc']-u['pdisch'])
+        return x0
+
+    def next_state(self, t, x, u, dt):
+        Todot = 1/self.parameters['mcOil'] * (self.parameters['HOil1']*(x['Tt']-x['To']) + self.parameters['HOil2']*(x['Tr']-x['To']) + self.parameters['HOil3']*(u['Tamb']-x['To']))
+        Ttdot = 1/self.parameters['mcThrust'] * (x['rThrust']*x['w']*x['w'] - self.parameters['HThrust1']*(x['Tt']-u['Tamb']) - self.parameters['HThrust2']*(x['Tt']-x['To']))
+        Adot = -x['wA']*x['Q']*x['Q']
+        rRadialdot = x['wRadial']*x['rRadial']*x['w']*x['w']
+        rThrustdot = x['wThrust']*x['wThrust']*x['w']*x['w']
+        friction = (self.parameters['r']+x['rThrust']+x['rRadial'])*x['w']
+        QLeak = math.copysign(self.parameters['cLeak']*self.parameters['ALeak']*math.sqrt(abs(u['psuc']-u['pdisch'])), u['psuc']-u['pdisch'])
+        Trdot = 1/self.parameters['mcRadial'] * (x['rRadial']*x['w']*x['w'] - self.parameters['HRadial1']*(x['Tr']-u['Tamb']) - self.parameters['HRadial2']*(x['Tr']-x['To']))
+        slipn = (u['wsync']-x['w'])/(u['wsync'])
+        ppump = x['A']*x['w']*x['w'] + self.parameters['b']*x['w']*x['Q']
+        Qout = max(0,x['Q']-QLeak)
+        slip = max(-1,(min(1,slipn)))
+        deltaP = ppump+u['psuc']-u['pdisch']
+        Te = 3*self.parameters['R2']/slip/(u['wsync']+0.00001)*u['V']**2/((self.parameters['R1']+self.parameters['R2']/slip)**2+(u['wsync']*self.parameters['L1'])**2)
+        backTorque = -self.parameters['a2']*Qout**2 + self.parameters['a1']*x['w']*Qout + self.parameters['a0']*x['w']**2
+        Qo = math.copysign(self.parameters['c']*math.sqrt(abs(deltaP)), deltaP)
+        wdot = (Te-friction-backTorque)/self.parameters['I']
+        Qdot = 1/self.parameters['FluidI']*(Qo-x['Q'])
+        QLeak = math.copysign(self.parameters['cLeak']*self.parameters['ALeak']*math.sqrt(abs(u['psuc']-u['pdisch'])), u['psuc']-u['pdisch'])
+
+
+        return {
+            'A': x['A'] + Adot*dt,
+            'Q': x['Q'] + Qdot*dt, 
+            'To': x['To'] + Todot*dt,
+            'Tr': x['Tr'] + Trdot*dt,
+            'Tt': x['Tt'] + Ttdot*dt,
+            'rRadial': x['rRadial'] + rRadialdot*dt,
+            'rThrust': x['rThrust'] + rThrustdot*dt,
+            'w': x['w']+wdot*dt,
+            'wA': x['wA'],
+            'wRadial': x['wRadial'],
+            'wThrust': x['wThrust'],
+            'QLeak': QLeak
+        }
+
+    def output(self, t, x):
+        Qout = max(0,x['Q']-x['QLeak'])
+
+        return {
+            'Qout': Qout,
+            'To': x['To'],
+            'Tr': x['Tr'],
+            'Tt': x['Tt'],
+            'w': x['w']
+        }
+
+    def event_state(self, t, x):
+        return {
+            'ImpellerWearFailure': (x['A'] - self.parameters['lim']['A'])/(self.parameters['x0']['A'] - self.parameters['lim']['A']),
+            'ThrustBearingOverheat': (self.parameters['lim']['Tt'] - x['Tt'])/(self.parameters['x0']['Tt'] - self.parameters['lim']['Tt']),
+            'RadialBearingOverheat': (self.parameters['lim']['Tr'] - x['Tr'])/(self.parameters['x0']['Tr'] - self.parameters['lim']['Tr']),
+            'PumpOilOverheat': (self.parameters['lim']['To'] - x['To'])/(self.parameters['x0']['To'] - self.parameters['lim']['To'])
+        }
+
+    def threshold_met(self, t, x):
+        return {
+            'ImpellerWearFailure': x['A'] > self.parameters['lim']['A'],
+            'ThrustBearingOverheat': x['Tt'] < self.parameters['lim']['Tt'],
+            'RadialBearingOverheat': x['Tr'] < self.parameters['lim']['Tr'],
+            'PumpOilOverheat': x['To'] < self.parameters['lim']['To']
+        }
