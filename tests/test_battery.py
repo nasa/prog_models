f51d59c1cb165a8c0b43546521f00028f593a950
Christopher Teubert <christopher.a.teubert@nasa.gov>
2020-12-22 15:48:30 -0800

    Add battery tests

--- tests/test_battery.py
+++ tests/test_battery.py
@@ -4,11 +4,27 @@ import unittest
 from prog_models.models.battery_circuit import BatteryCircuit
 from prog_models.models.battery_electrochem import BatteryElectroChem
 
+def future_loading(t):
+    # Variable (piece-wise) future loading scheme 
+    if (t < 600):
+        i = 2
+    elif (t < 900):
+        i = 1
+    elif (t < 1800):
+        i = 4
+    elif (t < 3000):
+        i = 2
+    else:
+        i = 3
+    return {'i': i}
+
 class TestBattery(unittest.TestCase):
     def test_battery_circuit(self):
         batt = BatteryCircuit()
+        (times, inputs, states, outputs, event_states) = batt.simulate_to(200, future_loading, {'t': 18.95, 'v': 4.183})
         # TODO(CT): More
 
     def test_battery_electrochem(self):
         batt = BatteryElectroChem()
+        (times, inputs, states, outputs, event_states) = batt.simulate_to(200, future_loading, {'t': 18.95, 'v': 4.183})
         # TODO(CT): More
