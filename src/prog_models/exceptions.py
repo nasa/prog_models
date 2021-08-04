72d3dca177a6efc452844c02768e2e962636528a
Wade Lamberson <wadelamberson55@gmail.com>
2021-08-04 19:16:29 -0400

    Yet Another Correction

--- src/prog_models/exceptions.py
+++ src/prog_models/exceptions.py
@@ -5,18 +5,15 @@ class ProgModelException(Exception):
     """
     Base Prognostics Model Exception
     """
-    pass
 
 
 class ProgModelInputException(ProgModelException):
     """
     Prognostics Input Exception - indicates the method input parameters were incorrect
     """
-    pass
 
 
 class ProgModelTypeError(ProgModelException, TypeError):
     """
     Prognostics Type Error - indicates the model could not be constructed
     """
-    pass
