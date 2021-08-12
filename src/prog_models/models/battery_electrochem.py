d4aba4e6aa0980c15225c6fcdee32a526318dc15
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-08-12 11:37:50 -0700

    Fix states in ElectroChem Model

--- src/prog_models/models/battery_electrochem.py
+++ src/prog_models/models/battery_electrochem.py
@@ -442,8 +442,8 @@ class BatteryElectroChemEODEOL(BatteryElectroChemEOL, BatteryElectroChemEOD):
     Inputs/Loading: (1)
         i: Current draw on the battery
 
-    States: (3)
-        qMax, Ro, D
+    States: (11)
+        tb, Vo, Vsn, Vsp, qnB, qnS, qpB, qpS, qMax, Ro, D
 
     Outputs/Measurements: (2)
         | t: Temperature of battery (°C) 
