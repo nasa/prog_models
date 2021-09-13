083871130730bb353914768a38c115028a0d9542
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-09-13 15:58:49 -0700

    added examples

--- examples/state_limits.py
+++ examples/state_limits.py
@@ -57,6 +57,13 @@ def run_example():
     print('Example 3')
     (times, inputs, states, outputs, event_states) = m.simulate_to_threshold(future_load, {'x':m.parameters['thrower_height']}, threshold_keys=[event], dt=0.005, save_freq=0.3, x = x0, print = True)
 
+    # Note that the limits can also be applied manually using the apply_limits function
+    print('limiting states')
+    x = {'x': -5, 'v': 3e8}  # Too fast and below the ground
+    print('\t Pre-limit: {}'.format(x))
+    x = m.apply_limits(x)
+    print('\t Post-limit: {}'.format(x))
+
 # This allows the module to be executed directly 
 if __name__=='__main__':
     run_example()
