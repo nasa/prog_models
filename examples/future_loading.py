a56a4989f085fafbf33a9836e0065509b525d190
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-12-16 15:55:28 -0800

    Switch to updated plot (example)

--- examples/future_loading.py
+++ examples/future_loading.py
@@ -7,7 +7,6 @@ Example demonstrating ways to use future loading.
 
 from prog_models.models import BatteryCircuit
 from statistics import mean
-from prog_models.visualize import plot_timeseries
 from numpy.random import normal
 
 def run_example(): 
@@ -35,8 +34,8 @@ def run_example():
     (times, inputs, states, outputs, event_states) = m.simulate_to_threshold(future_loading, **options)
 
     # Now lets plot the inputs and event_states
-    plot_timeseries(times, inputs, options={'ylabel': 'Variable Load Current (amps)'})
-    plot_timeseries(times, event_states, options={'ylabel': 'Variable Load Event State'})
+    inputs.plot(ylabel = 'Variable Load Current (amps)')
+    event_states.plot(ylabel = 'Variable Load Event State')
 
     ## Example 2: Moving Average loading 
     # This is useful in cases where you are running reoccuring simulations, and are measuring the actual load on the system, 
@@ -71,8 +70,8 @@ def run_example():
     (times, inputs, states, outputs, event_states) = m.simulate_to_threshold(future_loading, **options)
 
     # Now lets plot the inputs and event_states
-    plot_timeseries(times, inputs, options={'ylabel': 'Moving Average Current (amps)'})
-    plot_timeseries(times, event_states, options={'ylabel': 'Moving Average Event State'})
+    inputs.plot(ylabel = 'Moving Average Current (amps)')
+    event_states.plot(ylabel = 'Moving Average Event State')
 
     # In this case, this estimate is wrong because loading will not be steady, but at least it would give you an approximation.
 
@@ -101,8 +100,8 @@ def run_example():
     (times, inputs, states, outputs, event_states) = m.simulate_to_threshold(future_loading, **options)
 
     # Now lets plot the inputs and event_states
-    plot_timeseries(times, inputs, options={'ylabel': 'Variable Gaussian Current (amps)'})
-    plot_timeseries(times, event_states, options={'ylabel': 'Variable Gaussian Event State'})
+    inputs.plot(ylabel = 'Variable Gaussian Current (amps)')
+    event_states.plot(ylabel = 'Variable Gaussian Event State')
 
     # Example 4: Gaussian- increasing with time
     # For this we're using moving average. This is realistic because the further out from current time you get, 
@@ -140,8 +139,8 @@ def run_example():
     (times, inputs, states, outputs, event_states) = m.simulate_to_threshold(future_loading, **options)
 
     # Now lets plot the inputs and event_states
-    plot_timeseries(times, inputs, options={'ylabel': 'Moving Average Current (amps)'})
-    plot_timeseries(times, event_states, options={'ylabel': 'Moving Average Event State'})
+    inputs.plot(ylabel = 'Moving Average Current (amps)')
+    event_states.plot(ylabel = 'Moving Average Event State')
     
     # In this example future_loading.t has to be updated with current time before each prediction.
     
@@ -162,8 +161,8 @@ def run_example():
     (times, inputs, states, outputs, event_states) = m.simulate_to_threshold(future_loading, **options)
 
     # Now lets plot the inputs and event_states
-    plot_timeseries(times, inputs, options={'ylabel': 'Moving Average Current (amps)'})
-    plot_timeseries(times, event_states, options={'ylabel': 'Moving Average Event State'})
+    inputs.plot(ylabel = 'Moving Average Current (amps)')
+    event_states.plot(ylabel = 'Moving Average Event State')
 
     # In this example future_loading.t has to be updated with current time before each prediction.
 
