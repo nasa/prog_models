25eca4a1dfb7c0ceaddce14599516155bd0c9db5
Lawrence Hwang <lawrenceh326@gmail.com>
2022-02-02 09:55:41 -0800

    Improved property F checking
    If F is not None, perform our matrix check. If F is None, ensure an event_state function is specified. Otherwise, raise an AttributeError

--- src/prog_models/linear_model.py
+++ src/prog_models/linear_model.py
@@ -44,6 +44,8 @@ class LinearModel(PrognosticsModel, ABC):
         self._propertyCheck(self.G, len(self.events), 1, ["G","events","1"])
         if (self.F is not None):
             self._propertyCheck(self.F, len(self.events), len(self.states), ["F","events","states"])
+        elif (self.F is None and not callable(getattr(self, 'event_state'))): # F is None and no event_state method
+            raise AttributeError("Matrix type check failed: @property F must be of type None or have an event state function specified.")
 
     def _propertyCheck(self, matrix, rowsCount, colsCount, notes):
         """
