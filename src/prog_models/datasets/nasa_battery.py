58cd457edbb2fa95519bf5e430f7201f352364d2
Christopher Teubert <christopher.a.teubert@nasa.gov>
2022-02-23 09:41:42 -0800

    Remove unused import

--- src/prog_models/datasets/nasa_battery.py
+++ src/prog_models/datasets/nasa_battery.py
@@ -1,6 +1,5 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
-from collections.abc import Iterable
 import io
 import requests
 import numpy as np
