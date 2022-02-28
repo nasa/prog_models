16a5451c625438a2735302cc5e78e090f2872576
Katy <kjjarvis@ucdavis.edu>
2022-02-28 13:37:09 -0800

    Deletion of outdated attempt at DMD

--- examples/surrogate_DMD_test.py
+++ examples/surrogate_DMD_test.py
@@ -90,42 +90,8 @@ def run_example_DMD_dx():
     print('Simulation Time from DMD (in next_state): {} ms/sim'.format(timeDMD*2))
 
 
-def run_example_DMD(): 
-    batt = DMDModel()
-    batt.parameters['process_noise'] = 0
+# def run_example_DMD(): 
 
-    ## Define Loading Profile 
-    def future_loading(t, x=None):
-        if (t < 500):
-            i = 3
-        elif (t < 1000):
-            i = 1
-        elif (t < 2000):
-            i = 2
-        else:
-            i = 4
-        return {'i': i}
-
-    ## Simulation Options
-    options = {
-        'save_freq': 1,  # Frequency at which results are saved
-        'dt': 1  # Timestep
-    }
-
-    ## Simulate to Threshold: 
-    # simulated_results = batt.simulate_to_threshold(future_loading, **options)
-
-    ## Simulate to specific time: 
-    # simulated_results = batt.simulate_to(600, future_loading, **options)
-
-    # Benchmarking
-    print('Benchmarking:')
-    def sim_DMD():  
-        simulated_results = batt.simulate_to_threshold(future_loading, **options)
-    timeDMD = timeit(sim_DMD, number=100)
-
-    # Print results
-    print('Simulation Time from DMD: {} ms/sim'.format(timeDMD*2))
 
 
 # This allows the module to be executed directly 
@@ -136,6 +102,4 @@ if __name__ == '__main__':
     # Run DMD approximation in next_state
     # run_example_DMD_dx()
 
-    # Run Full DMD approximation 
-    # run_example_DMD()
     
