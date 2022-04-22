f43a70702c668c92dc7709e467fb728b07b84cbf
Lawrence Hwang <lawrenceh326@gmail.com>
2022-04-21 20:30:05 -0700

    Failing dataset tests for new error message
    Only passes when connection is off because URL cannot be passed in

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -3,6 +3,21 @@ import sys
 import unittest
 
 class TestDatasets(unittest.TestCase):
+    # TEST BATTERY ERROR EMSSAGE
+    # Need internet connection off to pass
+    # Can't pass in gibberish URL because load_data maps an int to a url: can't set url externally of function
+    
+    def test_nasa_battery_download_error(self):
+        from prog_models.datasets import nasa_battery
+        with self.assertRaises(ConnectionError):
+            # RW1 = "https://ti.arc.nasa.gov/c/27/" - a functional URL
+            (desc, data) = nasa_battery.load_data(1)
+    def test_nasa_cmapss_download_error(self):
+        from prog_models.datasets import nasa_cmapss
+        with self.assertRaises(ConnectionError):
+            # URL = "https://ti.arc.nasa.gov/c/6/" - a functional URL
+            (desc, data) = nasa_cmapss.load_data(1)
+
     def test_nasa_cmpass_download_error(self):
         pass
 
