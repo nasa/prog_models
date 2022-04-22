142a942ece24be3959cab17b5da5e4fda510c81f
Lawrence Hwang <lawrenceh326@gmail.com>
2022-04-22 11:31:44 -0700

    Move manual tests into separate file

--- tests/test_manual.py
+++ tests/test_manual.py
@@ -3,20 +3,16 @@
 import sys
 import unittest
 
-class TestDatasets(unittest.TestCase):
-    # Bad URL tests
-    def test_nasa_battery_bad_url_download(self):
+class TestManual(unittest.TestCase):
+    # Test successful download of datasets
+    def test_nasa_battery_download(self):
         from prog_models.datasets import nasa_battery
-        BAD_URL = "BADURLTEST"
-        nasa_battery.urls = {'RW1':"https://"+BAD_URL}
-        with self.assertRaises(ConnectionError):
-            (desc, data) = nasa_battery.load_data(1)
-    def test_nasa_cmapss_bad_url_download(self):
+        # RW1 = "https://ti.arc.nasa.gov/c/27/" - a functional URL
+        (desc, data) = nasa_battery.load_data(1) # is simply passing okay? a lot of values returned from desc, data
+    def test_nasa_cmapss_download(self):
         from prog_models.datasets import nasa_cmapss
-        BAD_URL = "https://"+"BADURLTEST"
-        nasa_cmapss.URL = BAD_URL
-        with self.assertRaises(ConnectionError):
-            (desc, data) = nasa_cmapss.load_data(1)
+        # URL = "https://ti.arc.nasa.gov/c/6/" - a functional URL
+        (desc, data) = nasa_cmapss.load_data(1)
 
 # This allows the module to be executed directly
 def run_tests():
@@ -29,11 +25,12 @@ def main():
 
     l = unittest.TestLoader()
     runner = unittest.TextTestRunner()
-    print("\n\nTesting Datasets")
-    result = runner.run(l.loadTestsFromTestCase(TestDatasets)).wasSuccessful()
+    print("\n\nTesting Manual")
+    result = runner.run(l.loadTestsFromTestCase(TestManual)).wasSuccessful()
 
     if not result:
         raise Exception("Failed test")
 
 if __name__ == '__main__':
     main()
+
