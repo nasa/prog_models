e411378c0edb4e116543ef8f8b1f4d758783e0e5
Christopher Teubert <christopher.a.teubert@nasa.gov>
2022-05-06 08:54:04 -0700

    Update surrogate tests

--- tests/test_surrogates.py
+++ tests/test_surrogates.py
@@ -1,6 +1,7 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
 import unittest
+import warnings
 
 from prog_models.models import *
 from prog_models.exceptions import ProgModelInputException
@@ -48,6 +49,116 @@ class TestSurrogate(unittest.TestCase):
         surrogate = m.generate_surrogate([load_eqn], dt = 0.1, save_freq = 0.25, threshold_keys = 'impact')
         self.assertEqual(surrogate.dt, 0.25)
 
+        self.assertListEqual(surrogate.states, m.states + m.outputs + m.events)
+        self.assertListEqual(surrogate.inputs, m.inputs)
+        self.assertListEqual(surrogate.outputs, m.outputs)
+        self.assertListEqual(surrogate.events, m.events)
+
+        options = {
+            'threshold_keys': 'impact',
+            'save_freq': 0.25,
+            'dt': 0.25
+        }
+        result = m.simulate_to_threshold(load_eqn, **options)
+        surrogate_results = surrogate.simulate_to_threshold(load_eqn, **options)
+
+        MSE = m.calc_error(surrogate_results.times, surrogate_results.inputs, surrogate_results.outputs)
+        self.assertLess(MSE, 10) # Pretty good approx
+
+        self.assertAlmostEqual(surrogate_results.times[-1], result.times[-1], delta=0.26)
+        for i in range(min(len(result.times), len(surrogate_results.times))):
+            self.assertListEqual(list(surrogate_results.inputs[i].keys()), list(surrogate_results.inputs[i].keys()))
+            self.assertAlmostEqual(surrogate_results.states[i]['x'], result.states[i]['x'], delta=8)
+            self.assertEqual(surrogate_results.states[i]['x'], surrogate_results.outputs[i]['x'])
+            self.assertAlmostEqual(surrogate_results.states[i]['v'], result.states[i]['v'], delta=1)
+            self.assertAlmostEqual(surrogate_results.states[i]['falling'], result.event_states[i]['falling'], delta=0.1)
+            self.assertEqual(surrogate_results.states[i]['falling'], surrogate_results.event_states[i]['falling'])
+            self.assertAlmostEqual(surrogate_results.states[i]['impact'], result.event_states[i]['impact'], delta=0.1)
+            self.assertEqual(surrogate_results.states[i]['impact'], surrogate_results.event_states[i]['impact'])
+
+    def test_surrogate_basic_battery(self):
+        m = BatteryElectroChem(process_noise = 0, measurement_noise = 0)
+        def future_loading_1(t, x=None):
+            # Variable (piece-wise) future loading scheme 
+            if (t < 500):
+                i = 3
+            elif (t < 1000):
+                i = 2
+            elif (t < 1500):
+                i = 0.5
+            else:
+                i = 4.5
+            return m.InputContainer({'i': i})
+        
+        def future_loading_2(t, x=None):
+            # Variable (piece-wise) future loading scheme 
+            if (t < 300):
+                i = 2
+            elif (t < 800):
+                i = 3.5
+            elif (t < 1300):
+                i = 4
+            elif (t < 1600):
+                i = 1.5
+            else:
+                i = 5
+            return m.InputContainer({'i': i})
+        load_functions = [future_loading_1, future_loading_2]
+
+        options_surrogate = {
+            'save_freq': 1, # For DMD, this value is the time step for which the surrogate model is generated
+            'dt': 0.1, # For DMD, this value is the time step of the training data
+            'trim_data_to': 0.7 # Value between 0 and 1 that determines the fraction of data resulting from simulate_to_threshold that is used to train DMD surrogate model
+        }
+
+        surrogate = m.generate_surrogate(load_functions,**options_surrogate)
+        self.assertListEqual(surrogate.states, m.states + m.outputs + m.events)
+        self.assertListEqual(surrogate.inputs, m.inputs)
+        self.assertListEqual(surrogate.outputs, m.outputs)
+        self.assertListEqual(surrogate.events, m.events)
+
+        options_sim = {
+            'save_freq': 1 # Frequency at which results are saved, or equivalently time step in results
+        }
+
+        # Define loading profile 
+        def future_loading(t, x=None):
+            if (t < 600):
+                i = 3
+            elif (t < 1000):
+                i = 2
+            elif (t < 1500):
+                i = 1.5
+            else:
+                i = 4
+            return m.InputContainer({'i': i})
+
+        result = m.simulate_to_threshold(future_loading, **options_sim)
+        surrogate_results = surrogate.simulate_to_threshold(future_loading,**options_sim)
+
+        self.assertAlmostEqual(surrogate_results.times[-1], result.times[-1], delta=50)
+        MSE = m.calc_error(surrogate_results.times, surrogate_results.inputs, surrogate_results.outputs)
+        self.assertLess(MSE, 300000)
+
+        STATE_DELTA_PERCENT = 0.05  # Percent deviation acceptable for state
+        for i in range(min(len(result.times), len(surrogate_results.times))):
+            for input in m.inputs:
+                self.assertAlmostEqual(surrogate_results.inputs[i][input], result.inputs[i][input], delta=0.1)
+            for state in m.states:
+                self.assertAlmostEqual(surrogate_results.states[i][state], result.states[i][state], delta=max(result.states[i][state]*STATE_DELTA_PERCENT, 0.25))
+            for event in m.events:
+                self.assertAlmostEqual(surrogate_results.event_states[i][event], result.event_states[i][event], delta=0.1)
+            self.assertAlmostEqual(surrogate_results.outputs[i]['v'], result.outputs[i]['v'], delta=0.75)
+    
+    def test_surrogate_subsets(self):
+        m = ThrownObject()
+        def load_eqn(t = None, x = None):
+            return m.InputContainer({})
+
+        # Perfect subset
+        surrogate = m.generate_surrogate([load_eqn], dt = 0.1, save_freq = 0.25, threshold_keys = 'impact', states=['x', 'v'])
+        self.assertEqual(surrogate.dt, 0.25)
+
         self.assertListEqual(surrogate.states, m.states + m.outputs + m.events + m.inputs)
         self.assertListEqual(surrogate.inputs, m.inputs)
         self.assertListEqual(surrogate.outputs, m.outputs)
@@ -62,7 +173,7 @@ class TestSurrogate(unittest.TestCase):
         surrogate_results = surrogate.simulate_to_threshold(load_eqn, **options)
         self.assertAlmostEqual(surrogate_results.times[-1], result.times[-1], delta=0.26)
         for i in range(min(len(result.times), len(surrogate_results.times))):
-            self.assertEqual(surrogate_results.inputs[i], surrogate_results.inputs[i])
+            self.assertListEqual(list(surrogate_results.inputs[i].keys()), list(surrogate_results.inputs[i].keys()))
             self.assertAlmostEqual(surrogate_results.states[i]['x'], result.states[i]['x'], delta=8)
             self.assertEqual(surrogate_results.states[i]['x'], surrogate_results.outputs[i]['x'])
             self.assertAlmostEqual(surrogate_results.states[i]['v'], result.states[i]['v'], delta=1)
@@ -70,6 +181,108 @@ class TestSurrogate(unittest.TestCase):
             self.assertEqual(surrogate_results.states[i]['falling'], surrogate_results.event_states[i]['falling'])
             self.assertAlmostEqual(surrogate_results.states[i]['impact'], result.event_states[i]['impact'], delta=0.1)
             self.assertEqual(surrogate_results.states[i]['impact'], surrogate_results.event_states[i]['impact'])
+        
+        # State subset
+        surrogate = m.generate_surrogate([load_eqn], dt = 0.1, save_freq = 0.25, threshold_keys = 'impact', states = ['v'])
+        self.assertListEqual(surrogate.states, ['v'] + m.outputs + m.events + m.inputs)
+        self.assertListEqual(surrogate.inputs, m.inputs)
+        self.assertListEqual(surrogate.outputs, m.outputs)
+        self.assertListEqual(surrogate.events, m.events)
+
+        surrogate = m.generate_surrogate([load_eqn], dt = 0.1, save_freq = 0.25, threshold_keys = 'impact', states = 'v')
+        self.assertListEqual(surrogate.states, ['v'] + m.outputs + m.events + m.inputs)
+        self.assertListEqual(surrogate.inputs, m.inputs)
+        self.assertListEqual(surrogate.outputs, m.outputs)
+        self.assertListEqual(surrogate.events, m.events)
+
+        options = {
+            'threshold_keys': 'impact',
+            'save_freq': 0.25,
+            'dt': 0.25
+        }
+        result = m.simulate_to_threshold(load_eqn, **options)
+        surrogate_results = surrogate.simulate_to_threshold(load_eqn, **options)
+        self.assertAlmostEqual(surrogate_results.times[-1], result.times[-1], delta=0.26)
+        for i in range(min(len(result.times), len(surrogate_results.times))):
+            self.assertListEqual(list(surrogate_results.inputs[i].keys()), list(surrogate_results.inputs[i].keys()))
+            self.assertAlmostEqual(surrogate_results.states[i]['x'], result.states[i]['x'], delta=8)
+            self.assertEqual(surrogate_results.states[i]['x'], surrogate_results.outputs[i]['x'])
+            self.assertAlmostEqual(surrogate_results.states[i]['v'], result.states[i]['v'], delta=1)
+            self.assertAlmostEqual(surrogate_results.states[i]['falling'], result.event_states[i]['falling'], delta=0.1)
+            self.assertEqual(surrogate_results.states[i]['falling'], surrogate_results.event_states[i]['falling'])
+            self.assertAlmostEqual(surrogate_results.states[i]['impact'], result.event_states[i]['impact'], delta=0.1)
+            self.assertEqual(surrogate_results.states[i]['impact'], surrogate_results.event_states[i]['impact'])
+
+        # Events subset
+        surrogate = m.generate_surrogate([load_eqn], dt = 0.1, save_freq = 0.25,      threshold_keys = 'impact', events = ['impact'])
+        surrogate = m.generate_surrogate([load_eqn], dt = 0.1, save_freq = 0.25,      threshold_keys = 'impact', events = 'impact')
+        self.assertListEqual(surrogate.states, m.states + m.outputs + ['impact'] + m.inputs)
+        self.assertListEqual(surrogate.inputs, m.inputs)
+        self.assertListEqual(surrogate.outputs, m.outputs)
+        self.assertListEqual(surrogate.events, ['impact'])
+
+        options = {
+            'threshold_keys': 'impact',
+            'save_freq': 0.25,
+            'dt': 0.25
+        }
+        result = m.simulate_to_threshold(load_eqn, **options)
+        surrogate_results = surrogate.simulate_to_threshold(load_eqn, **options)
+        self.assertAlmostEqual(surrogate_results.times[-1], result.times[-1], delta=0.26)
+        for i in range(min(len(result.times), len(surrogate_results.times))):
+            self.assertListEqual(list(surrogate_results.inputs[i].keys()), list(surrogate_results.inputs[i].keys()))
+            self.assertAlmostEqual(surrogate_results.states[i]['x'], result.states[i]['x'], delta=8)
+            self.assertEqual(surrogate_results.states[i]['x'], surrogate_results.outputs[i]['x'])
+            self.assertAlmostEqual(surrogate_results.states[i]['v'], result.states[i]['v'], delta=1.25)
+            self.assertAlmostEqual(surrogate_results.states[i]['impact'], result.event_states[i]['impact'], delta=0.1)
+            self.assertEqual(surrogate_results.states[i]['impact'], surrogate_results.event_states[i]['impact'])
+
+        # Outputs - Empty
+        surrogate = m.generate_surrogate([load_eqn], dt = 0.1, save_freq = 0.25,      threshold_keys = 'impact', outputs = [])
+        self.assertListEqual(surrogate.states, m.states + m.events + m.inputs)
+        self.assertListEqual(surrogate.inputs, m.inputs)
+        self.assertListEqual(surrogate.outputs, [])
+        self.assertListEqual(surrogate.events, m.events)
+
+        options = {
+            'threshold_keys': 'impact',
+            'save_freq': 0.25,
+            'dt': 0.25
+        }
+        result = m.simulate_to_threshold(load_eqn, **options)
+        surrogate_results = surrogate.simulate_to_threshold(load_eqn, **options)
+        self.assertAlmostEqual(surrogate_results.times[-1], result.times[-1], delta=0.26)
+        for i in range(min(len(result.times), len(surrogate_results.times))):
+            self.assertListEqual(list(surrogate_results.inputs[i].keys()), list(surrogate_results.inputs[i].keys()))
+            self.assertAlmostEqual(surrogate_results.states[i]['x'], result.states[i]['x'], delta=8)
+            self.assertAlmostEqual(surrogate_results.states[i]['v'], result.states[i]['v'], delta=1)
+            self.assertAlmostEqual(surrogate_results.states[i]['falling'], result.event_states[i]['falling'], delta=0.1)
+            self.assertEqual(surrogate_results.states[i]['falling'], surrogate_results.event_states[i]['falling'])
+            self.assertAlmostEqual(surrogate_results.states[i]['impact'], result.event_states[i]['impact'], delta=0.1)
+            self.assertEqual(surrogate_results.states[i]['impact'], surrogate_results.event_states[i]['impact'])
+
+    def test_surrogate_options(self):
+        m = ThrownObject()
+        def load_eqn(t = None, x = None):
+            return m.InputContainer({})
+        
+        # treat warnings as exceptions
+        warnings.filterwarnings("error")
+
+        try:
+            surrogate = m.generate_surrogate([load_eqn], dt = 0.1, save_freq = 0.25, threshold_keys = 'impact', states = ['v'])
+            self.fail('warning not raised')
+        except Warning:
+            pass
+
+        try:
+            # Set sufficiently large failure tolerance
+            surrogate = m.generate_surrogate([load_eqn], dt = 0.1, save_freq = 0.25, threshold_keys = 'impact', states = ['v'], stability_tol=1e99)
+        except Warning:
+            self.fail('Warning raised')
+        
+        # Reset Warnings
+        warnings.filterwarnings("default")
 
     def test_surrogate_use_error_cases(self):
         m = ThrownObject()
