f546bbc6495a1af1e97b313b2e3be0961de14ac5
Lawrence Hwang <lawrenceh326@gmail.com>
2022-05-09 10:15:49 -0700

    Moving bin first line

--- scripts/gen_docs.sh
+++ scripts/gen_docs.sh
@@ -1,6 +1,6 @@
+#!/bin/sh
 # Copyright © 2022 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
-#!/bin/sh
 # Update documentation. Note you must have sphinx installed
 #  run pip install sphinx to install sphinx
 python -m sphinx sphinx_config docs
