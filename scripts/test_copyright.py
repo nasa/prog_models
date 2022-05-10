f75d11737b4ce38af04bc1c0e7621b494bee7b5d
Lawrence Hwang <lawrenceh326@gmail.com>
2022-05-10 11:27:15 -0700

    Testing structure

--- scripts/test_copyright.py
+++ scripts/test_copyright.py
@@ -1,3 +1,12 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the
 # National Aeronautics and Space Administration.  All Rights Reserved.
 
+def main():
+    print("\n\nTesting Files for Copyright")
+    
+    # if fail:
+    #     raise Exception("Failed test")
+
+if __name__ == '__main__':
+    main()
+
