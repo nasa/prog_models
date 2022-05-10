8e43b7ce151bfc7d7cd508e54440a15af3384290
Christopher Teubert <christopher.a.teubert@nasa.gov>
2022-05-10 13:08:26 -0700

    Copyright date fix

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,4 +1,4 @@
-# Copyright © 2020 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
+# Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
 from .test_base_models import main as base_models_main
 from .test_sim_result import main as sim_result_main
