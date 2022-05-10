cf1cb1a596f04561b7ccafbdbb15d9c68e7e8379
Christopher Teubert <christopher.a.teubert@nasa.gov>
2022-05-10 10:00:19 -0700

    Cleaning tutorial, example

--- examples/benchmarking.py
+++ examples/benchmarking.py
@@ -14,23 +14,13 @@ def run_example():
     
     # Step 2: Define future loading function 
     def future_loading(t, x=None):
-        # Variable (piece-wise) future loading scheme 
-        if (t < 600):
-            i = 2
-        elif (t < 900):
-            i = 1
-        elif (t < 1800):
-            i = 4
-        elif (t < 3000):
-            i = 2
-        else:
-            i = 3
-        return batt.InputContainer({'i': i})
+        # Constant Loading
+        return batt.InputContainer({'i': 2})
 
-    # Step 3: Benchmark simulation for 600 seconds
-    print('Benchmarking:')
+    # Step 3: Benchmark simulation of 600 seconds
+    print('Benchmarking...')
     def sim():  
-        (times, inputs, states, outputs, event_states) = batt.simulate_to(600, future_loading)
+        results = batt.simulate_to(600, future_loading)
     time = timeit(sim, number=500)
 
     # Print results
