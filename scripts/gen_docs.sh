f7c5c22ed5ce9cfac8720cedf939d499ada7bd29
Lawrence Hwang <lawrenceh326@gmail.com>
2022-05-10 10:40:23 -0700

    Correcting co for 2021

--- scripts/gen_docs.sh
+++ scripts/gen_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright © 2022 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
+# Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
 # Update documentation. Note you must have sphinx installed
 #  run pip install sphinx to install sphinx
