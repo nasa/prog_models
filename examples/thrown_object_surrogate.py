73125060745e4cb01233514de95346653d605945
Katy <kjjarvis@ucdavis.edu>
2022-05-17 16:58:36 -0700

    Bug fix with dict/containers and additional details on new ex

--- examples/thrown_object_surrogate.py
+++ examples/thrown_object_surrogate.py
@@ -13,7 +13,7 @@ def run_example():
     ### Example 1: Standard DMD Application 
     ## Step 1: Create a model object
     m = ThrownObject(process_noise = 0, measurement_noise = 0)
-    m.parameters['cd'] = 0.8
+    m.parameters['cd'] = 0.1
 
     ## Step 2: Define future loading functions for training data 
     # Here, we define two specific loading profiles. These could also be generated programmatically, for as many loading profiles as desired 
@@ -31,6 +31,7 @@ def run_example():
         'dt': 0.1, # For DMD, this value is the time step of the training data
         'threshold_keys': 'impact',
         'states': ['v'],
+        'events': [],
         'training_noise': 0 # 0.06
     }
 
@@ -41,8 +42,8 @@ def run_example():
     # Simulation options for implementation of surrogate model
     options_sim = {
         'save_freq': 0.1, # Frequency at which results are saved, or equivalently time step in results    
-        'threshold_keys': 'impact',
-        'dt': 0.1
+        # 'threshold_keys': 'impact',
+        # 'dt': 0.1
     }
     options_hf = {
         'dt': 0.1,
@@ -55,9 +56,10 @@ def run_example():
         return m.InputContainer({})
 
     # Simulate to threshold using DMD approximation
-    simulated_results = DMD_approx.simulate_to_threshold(future_loading,**options_sim)
+    # simulated_results = DMD_approx.simulate_to_threshold(future_loading,**options_sim)
+    simulated_results = DMD_approx.simulate_to(8,future_loading,**options_sim)
     # high_fidelity_results = m.simulate_to_threshold(future_loading, **options_hf)
-    high_fidelity_results = m.simulate_to_threshold(future_loading, **options_sim)
+    high_fidelity_results = m.simulate_to(8, future_loading, **options_hf)
 
     # Plot results
     # simulated_results.event_states.plot(title='Surrogate Model Event States')
@@ -71,15 +73,15 @@ def run_example():
     time_dmd = [simulated_results.times[iter] for iter in range(len(simulated_results.times))]
     velocity_dmd = [simulated_results.states[iter]['v'] for iter in range(len(simulated_results.times))]
     position_dmd = [simulated_results.states[iter]['x'] for iter in range(len(simulated_results.times))]
-    falling_dmd = [simulated_results.states[iter]['falling'] for iter in range(len(simulated_results.times))]
-    impact_dmd = [simulated_results.states[iter]['impact'] for iter in range(len(simulated_results.times))]
+    # falling_dmd = [simulated_results.states[iter]['falling'] for iter in range(len(simulated_results.times))]
+    # impact_dmd = [simulated_results.states[iter]['impact'] for iter in range(len(simulated_results.times))]
 
     # Extract HF results
     time_hf = [high_fidelity_results.times[iter] for iter in range(len(high_fidelity_results.times))]
     velocity_hf = [high_fidelity_results.states[iter]['v'] for iter in range(len(high_fidelity_results.times))]
     position_hf = [high_fidelity_results.states[iter]['x'] for iter in range(len(high_fidelity_results.times))]
-    falling_hf = [high_fidelity_results.event_states[iter]['falling'] for iter in range(len(high_fidelity_results.times))]
-    impact_hf = [high_fidelity_results.event_states[iter]['impact'] for iter in range(len(high_fidelity_results.times))]
+    # falling_hf = [high_fidelity_results.event_states[iter]['falling'] for iter in range(len(high_fidelity_results.times))]
+    # impact_hf = [high_fidelity_results.event_states[iter]['impact'] for iter in range(len(high_fidelity_results.times))]
 
     # Test if DMD and HF at same times 
     time_bool = [time_dmd[iter] == time_hf[iter] for iter in range(min(len(time_dmd),len(time_hf)))]
@@ -87,11 +89,12 @@ def run_example():
         print('Error: times are not equal.')
 
     # Calculate error:
-    rmse_impact = math.sqrt(sum([((impact_hf[iter] - impact_dmd[iter])**2)/len(impact_dmd) for iter in range(min(len(impact_dmd),len(impact_hf)))]))
-    rmse_falling = math.sqrt(sum([((falling_hf[iter] - falling_dmd[iter])**2)/len(falling_dmd) for iter in range(min(len(falling_dmd),len(falling_hf)))]))
-    rmse_position = math.sqrt(sum([((position_hf[iter] - position_dmd[iter])**2)/len(position_dmd) for iter in range(min(len(position_dmd),len(position_hf)))]))
-    rmse_velocity = math.sqrt(sum([((velocity_hf[iter] - velocity_dmd[iter])**2)/len(velocity_dmd) for iter in range(min(len(velocity_dmd),len(velocity_hf)))]))    
+    # rmse_impact = math.sqrt(sum([((impact_hf[iter] - impact_dmd[iter])**2)/min(len(impact_dmd),len(impact_hf)) for iter in range(min(len(impact_dmd),len(impact_hf)))]))
+    # rmse_falling = math.sqrt(sum([((falling_hf[iter] - falling_dmd[iter])**2)/min(len(falling_dmd),len(falling_hf)) for iter in range(min(len(falling_dmd),len(falling_hf)))]))
+    rmse_position = math.sqrt(sum([((position_hf[iter] - position_dmd[iter])**2)/min(len(position_dmd),len(position_hf)) for iter in range(min(len(position_dmd),len(position_hf)))]))
+    rmse_velocity = math.sqrt(sum([((velocity_hf[iter] - velocity_dmd[iter])**2)/min(len(velocity_dmd),len(velocity_hf)) for iter in range(min(len(velocity_dmd),len(velocity_hf)))]))    
 
+    """
     # plot
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
     ax1.plot(time_hf,position_hf,'-b',label='High fidelity')
@@ -103,6 +106,7 @@ def run_example():
     ax3.plot(time_hf,impact_hf,'-b',label='High fidelity')
     ax3.plot(time_dmd,impact_dmd,'--r',label='DMD')
     ax3.set_title('Impact')
+    """
 
     debug = 1
 
@@ -134,7 +138,7 @@ def run_comparison():
             'dt': 0.1, # For DMD, this value is the time step of the training data
             'threshold_keys': 'impact',
             'states': ['v'],
-            'events': [],
+            # 'events': [],
             'training_noise': 0 # 0.06
         }
 
@@ -151,7 +155,7 @@ def run_comparison():
         options_hf = {
             'dt': 0.1,
             'save_freq': 0.1,
-            # 'threshold_keys': 'impact'
+            'threshold_keys': 'impact'
         }
 
         # Define loading profile 
@@ -161,8 +165,8 @@ def run_comparison():
         # Simulate to threshold using DMD approximation
         # simulated_results = DMD_approx.simulate_to_threshold(future_loading,**options_sim)
         simulated_results = DMD_approx.simulate_to(8,future_loading,**options_sim)
-        # high_fidelity_results = m.simulate_to_threshold(future_loading, **options_hf)
-        high_fidelity_results = m.simulate_to(8, future_loading, **options_hf)
+        high_fidelity_results = m.simulate_to_threshold(future_loading, **options_hf)
+        # high_fidelity_results = m.simulate_to(8, future_loading, **options_hf)
 
         # Plot results
         # simulated_results.event_states.plot(title='Surrogate Model Event States')
@@ -192,11 +196,12 @@ def run_comparison():
             print('Error: times are not equal.')
 
         # Calculate error:
-        # rmse_impact.append(math.sqrt(sum([((impact_hf[iter] - impact_dmd[iter])**2)/len(impact_dmd) for iter in range(min(len(impact_dmd),len(impact_hf)))])))
-        # rmse_falling.append(math.sqrt(sum([((falling_hf[iter] - falling_dmd[iter])**2)/len(falling_dmd) for iter in range(min(len(falling_dmd),len(falling_hf)))])))
-        rmse_position.append(math.sqrt(sum([((position_hf[iter] - position_dmd[iter])**2)/len(position_dmd) for iter in range(min(len(position_dmd),len(position_hf)))])))
-        rmse_velocity.append(math.sqrt(sum([((velocity_hf[iter] - velocity_dmd[iter])**2)/len(velocity_dmd) for iter in range(min(len(velocity_dmd),len(velocity_hf)))])))  
+        # rmse_impact.append(math.sqrt(sum([((impact_hf[iter] - impact_dmd[iter])**2)/min(len(impact_dmd),len(impact_hf)) for iter in range(min(len(impact_dmd),len(impact_hf)))])))
+        # rmse_falling.append(math.sqrt(sum([((falling_hf[iter] - falling_dmd[iter])**2)/min(len(falling_dmd),len(falling_hf)) for iter in range(min(len(falling_dmd),len(falling_hf)))])))
+        rmse_position.append(math.sqrt(sum([((position_hf[iter] - position_dmd[iter])**2)/min(len(position_dmd),len(position_hf)) for iter in range(min(len(position_dmd),len(position_hf)))])))
+        rmse_velocity.append(math.sqrt(sum([((velocity_hf[iter] - velocity_dmd[iter])**2)/min(len(velocity_dmd),len(velocity_hf)) for iter in range(min(len(velocity_dmd),len(velocity_hf)))])))  
 
+        a = 1
         """
         # plot
         fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
--- src/prog_models/prognostics_model.py
+++ src/prog_models/prognostics_model.py
@@ -1163,10 +1163,10 @@ class PrognosticsModel(ABC):
         config.update(kwargs)
 
         # List of user-define values to include in surrogate model: 
-        states_dmd = deepcopy(self.states)
-        inputs_dmd = deepcopy(self.inputs)
-        outputs_dmd = deepcopy(self.outputs)
-        events_dmd = deepcopy(self.events)
+        states_dmd = self.states.copy()
+        inputs_dmd = self.inputs.copy()
+        outputs_dmd = self.outputs.copy()
+        events_dmd = self.events.copy()
 
         # Validate user inputs 
         try:
@@ -1437,7 +1437,14 @@ class PrognosticsModel(ABC):
                 if config['dt'] != None:
                     warn("dt is not used in DMD approximation")
 
-                if config['save_freq'] == dmd_dt and config['save_pts'] == []: 
+                if (
+                    config['save_freq'] == dmd_dt or
+                    (
+                        isinstance(config['save_freq'], tuple) and
+                        config['save_freq'][0]%dmd_dt < 1e-9 and
+                        config['save_freq'][1] == dmd_dt
+                    )
+                ) and config['save_pts'] == []:
                     # In this case, the user wants what the DMD approximation returns 
                     return results 
                 
@@ -1446,8 +1453,15 @@ class PrognosticsModel(ABC):
                 time_basic = [results.times[0], results.times[-1]]
                 time_basic.extend(config['save_pts'])                       
                 if config['save_freq'] != None:
-                    # Add Save Frequency
-                    time_array = np.arange(results.times[0]+config['save_freq'],results.times[-1],config['save_freq'])
+                    if isinstance(config['save_freq'], tuple):
+                        # Tuple used to specify start and frequency
+                        t_step = config['save_freq'][1]
+                        # Use starting time or the next multiple
+                        t_start = config['save_freq'][0]
+                        start = max(t_start, results.times[0] - (results.times[0]-t_start)%t_step)
+                        time_array = np.arange(start+t_step,results.times[-1],t_step)
+                    else: 
+                        time_array = np.arange(results.times[0]+config['save_freq'],results.times[-1],config['save_freq'])
                     time_basic.extend(time_array.tolist())
                 time_interp = sorted(time_basic)
 
