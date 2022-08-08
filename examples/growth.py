084675d88b03fcefae296024e191b743bd3cb7da
Henry Lembo <lembohenry@gmail.com>
2022-08-08 12:23:51 -0400

    Paris Law Crack Model

---
+++ examples/growth.py
@@ -0,0 +1,92 @@
+# Copyright © 2021 United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration.  All Rights Reserved.
+
+"""
+Example of a battery being simulated for a set period of time and then till threshold is met.
+"""
+
+from prog_models.models.paris_law import Growth 
+import matplotlib.pyplot as plt
+import csv # for reading csv files
+import pandas as pd
+import os
+# VVV Uncomment this to use Electro Chemistry Model VVV
+# from prog_models.models import BatteryElectroChem as Battery
+
+def run_example(): 
+    # Step 1: Create a model object
+    m = Growth(process_noise = 0)
+    
+    # Step 2: Define future loading function 
+    def future_loading(t, x=None):
+        # Variable (piece-wise) future loading scheme 
+        #inputs are ['k_min', 'k_max']
+        if (t < 500):
+            k_min = 12
+            k_max = 24
+        elif (t < 750):
+            k_min = 8
+            k_max = 32
+        else:
+            k_min = 0
+            k_max = 28
+        return m.InputContainer({'k_min': k_min, 'k_max': k_max})
+
+    
+    times = []
+    inputs = []
+    outputs = []
+    
+    #Finds file path
+
+    csv_dir = os.path.join(os.path.dirname(__file__), 'growth.csv')
+
+    #Reads csv file
+    try:
+        with open(csv_dir, newline='') as csvfile:
+            data = csv.reader(csvfile, delimiter=',', quotechar='|' , quoting=csv.QUOTE_NONNUMERIC)
+            for row in data:
+                times.append(row[0])
+                inputs.append({'k_min': row[1], 'k_max': row[2]})
+                outputs.append({'c_li': row[3]})
+    except FileNotFoundError:
+        print("No data file found")
+
+    # estimates the model parameters
+    keys = ['c', 'm']
+
+    print('Model configuration before')
+    for key in keys:
+        print("-", key, m.parameters[key])
+    print(' Error: ', m.calc_error(times, inputs, outputs, dt=10))
+
+    m.estimate_params([(times, inputs, outputs)], keys, dt=10)
+
+    print('\nOptimized configuration')
+    for key in keys:
+        print("-", key, m.parameters[key])
+    print(' Error: ', m.calc_error(times, inputs, outputs, dt=10))
+
+    # Simulate to threshold
+    print('\n\n------------------------------------------------')
+    print('Simulating to threshold\n\n')
+    options = {
+        'save_freq': 10, # Frequency at which results are saved
+        'dt': 10, # Timestep
+        'print': True,
+        'progress': False,
+        'horizon': 1e5, # Horizon
+    }
+
+    (times, inputs, states, outputs, event_states) = m.simulate_to_threshold(future_loading, **options)
+
+    # Step 4: Plot Results
+    # crack length
+    # plot event state
+
+    inputs.plot(ylabel='Stress Intensity')
+    event_states.plot(ylabel= 'CGF')
+    outputs.plot(ylabel= {'c_li': "Crack Length"}, compact= False)
+    plt.show()
+if __name__ == '__main__':
+    run_example()
---
+++ src/prog_models/models/paris_law.py
@@ -0,0 +1,154 @@
+# Copyright © 2021 United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration.  All Rights Reserved.
+
+# INSTRUCTIONS:
+# 1. Copy this file- renaming to the name of your model
+# 2. Rename the class as desired
+# 3. Replace the events, inputs, states, outputs keys with those specific to the model
+# 4. Uncomment either dx or next_state function. dx for continuous models, and next_state for discrete
+# 5. Implement logic of model in each method
+
+# Note: To preserve vectorization use numpy math function (e.g., maximum, minimum, sign, sqrt, etc.) instead of non-vectorized functions (max, min, etc.)
+
+from typing_extensions import Self
+from prog_models import PrognosticsModel
+from numpy import inf
+import numpy as np
+
+# REPLACE THIS WITH DERIVED PARAMETER CALLBACKS (IF ANY)
+# See examples.derived_params
+# 
+# Each function defines one or more derived parameters as a function of the other parameters.
+def example_callback(params):
+    # Return format: dict of key: new value pair for at least one derived parameter
+    return {
+        "Example Parameter 1": params["Example Parameter 2"]-3
+    }
+
+
+class Growth(PrognosticsModel): 
+    """
+    Events: (1)
+        CGF :   Crack Growth Fracture 
+
+    # Correct
+    Inputs/Loading: (2)
+      |k_max :  Maximum crack growth 
+      |k_min :  Minimum crack growth 
+
+    # Work in Progress
+    # Meets two criteria expects to be changing over time for a system as your simulate it
+    # something you need to keep track of in order to calculate the event state or outputs
+    # and progress towards the event (Crack Growth Fracture)
+    States: (1)
+      |c_li :    crack length # wrong Yes, Yes One state
+  
+    Outputs: (1) #output  user needs to be able to measure
+       |c_l :   crack length
+
+    Model Configuration Parameters:
+       |a :     Length of crack
+       |c :     Constant
+       |m :     Constant
+       |n :     cycles per loading
+    """ 
+
+    # V Uncomment Below if the class is vectorized (i.e., if it can accept input to all functions as arrays) V
+    # is_vectorized = True
+
+    # REPLACE THE FOLLOWING LIST WITH EVENTS BEING PREDICTED
+    events = ['CGF']
+    
+    # REPLACE THE FOLLOWING LIST WITH INPUTS (LOADING)
+    inputs = ['k_max', 'k_min'] # change
+
+    # REPLACE THE FOLLOWING LIST WITH STATES
+    states = ['c_li']
+
+    # REPLACE THE FOLLOWING LIST WITH OUTPUTS (MEASURED VALUES)
+    outputs = ['c_li']
+
+    # REPLACE THE FOLLOWING LIST WITH CONFIGURED PARAMETERS
+    default_parameters = {# Set default parameters
+        'config_length': 1e-4,
+        'c': 3.24,
+        'm': 0.1527,
+        'dndt': 10, 
+        'x0' : {
+            'c_li': 0.00001,
+        }  
+    }
+
+    # REPLACE THE FOLLOWING WITH STATE BOUNDS IF NEEDED
+    state_limits = {
+        # 'state': (lower_limit, upper_limit)
+        # only specify for states with limits
+        #crack length cannot be 0 else it is not a crack and cannot be infinite length
+        'c_li': (0, inf),
+    }
+
+    # Identify callbacks used by this model
+    # See examples.derived_params
+    # Format: "trigger": [callbacks]
+    # Where trigger is the parameter that the derived parameters are derived from.
+    # And callbacks are one or more callback functions that define parameters that are 
+    # derived from that parameter
+    # REPLACE THIS WITH ACTUAL DERIVED PARAMETER CALLBACKS
+    #param_callbacks = {
+    #    "Example Parameter 2": [example_callback]
+    #}
+
+
+    #def __init__(self, **kwargs):
+        
+        # ADD OPTIONS CHECKS HERE
+
+        # e.g., Checking for required parameters
+        # if not 'required_param' in kwargs: 
+        #   throw Exception;
+
+        # e.g. 2, Modify parameters
+        # kwargs['some_param'] = some_function(kwargs['some_param'])
+
+    #super().__init__(**kwargs) # Run Parent constructor
+
+    # Sometimes initial input (u) and initial output (z) are needed to initialize the model
+    # In that case remove the '= None' for the appropriate argument
+    # Note: If they are needed, that requirement propogated through to the simulate_to* functions
+    def initialize(self, u=None, z=None):
+        return self.StateContainer(self.parameters['x0'])
+
+    #- UNCOMMENT THIS FUNCTION FOR CONTINUOUS MODELS
+    # update crack length based of derivative of  da/dt
+    # cycle rate wont change
+    #get the derivative of the crack length
+    #multiply by the cycle rate
+    def dx(self, x : dict, u : dict):
+        parameters = self.parameters
+        r = (parameters['c']*(u['k_max'] - u['k_min'])**parameters['m'])*parameters['dndt']#r = C*(k_max / k_min)^m (Stress ratio)
+        dxdt = {
+             'c_li': r,
+         }
+        return self.StateContainer(dxdt)
+
+    def output(self, x):
+        return self.OutputContainer(x) #disctionary or an array/coloumn vector of outputs
+
+        # when crack length is maximum it should be 0
+        # when crack length is 0 it should be 1
+        # when the crack length is half the maximum it should be 0.5
+
+
+    def event_state(self, x : dict) -> dict: #true if it greater than the threashold
+       return {
+            'CGF' : 1- x['c_li'] / self.parameters['config_length']
+        }
+
+       
+    # Note: Thresholds met equation below is not strictly necessary. By default threshold_met will check if event_state is ≤ 0 for each event
+    # crack length ends when we reach a configurable size
+    def threshold_met(self, x):
+        t_met = {
+           'CGF': x['c_li'] > self.parameters['config_length']
+        }
+        return t_met 
