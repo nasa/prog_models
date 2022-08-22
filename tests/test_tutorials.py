01be0760eb23cfae58d513c2daa043e05b829395
Henry Lembo <lembohenry@gmail.com>
2022-08-22 16:08:07 -0400

    Updated Testbook Test

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -1,21 +1,30 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
 import unittest
-from testbook import testbook
+import warnings
+import importlib.util
+
 
 class TestTutorials(unittest.TestCase):
+
     def test_tutorial_ipynb(self):
-        with testbook('./tutorial.ipynb', execute=True) as tb:
-            self.assertEqual(tb.__class__.__name__, "TestbookNotebookClient")
+        if importlib.util.find_spec('testbook') is None:
+            warnings.warn('testbook not imported')
+        else:
+            with testbook('./tutorial.ipynb', execute=True) as tb:
+                from testbook import testbook
+                self.assertEqual(tb.__class__.__name__, "TestbookNotebookClient")
 
 def main():
     l = unittest.TestLoader()
     runner = unittest.TextTestRunner()
     print("\n\nTesting Tutorials")
-    result = runner.run(l.loadTestsFromTestCase(TestTutorials)).wasSuccessful()
-
-    if not result:
-        raise Exception("Failed test")
+    try:
+        result = runner.run(l.loadTestsFromTestCase(TestTutorials)).wasSuccessful()
+    except Exception as e:
+        if not result:
+            raise Exception("Failed test")
+          
 
 if __name__ == '__main__':
     main()
