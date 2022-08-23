103b6c388ac00edbcf6631c98872a53898947dfe
Christopher Teubert <christopher.a.teubert@nasa.gov>
2022-08-23 07:30:09 -0700

    Setup redirect

--- sphinx_config/conf.py
+++ sphinx_config/conf.py
@@ -34,7 +34,7 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', "sphinx.ext.githubpages", 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', "sphinx.ext.githubpages", 'sphinx.ext.napoleon', 'sphinx_reredirects']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -55,3 +55,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+redirects = {
+     "*": "https://nasa.github.io/progpy"
+}
