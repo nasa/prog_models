5240203cc394d96234f51abe05c22b5e7d9b6d71
Katy <kjjarvis@ucdavis.edu>
2022-09-27 16:54:11 -0700

    Moving initializations of x_ref, vehicle, etc. to under-the-hood

--- examples/trajectory_gen.py
+++ examples/trajectory_gen.py
@@ -1,71 +1,79 @@
 
 
-import prog_models.models.trajectory_generation.trajectory.route as route 
-import prog_models.models.trajectory_generation.trajectory.trajectory as trajectory
-from prog_models.models.trajectory_generation.vehicles import AircraftModels
-from prog_models.models.trajectory_generation.vehicles.control import allocation_functions
+# import prog_models.models.trajectory_generation.trajectory.route as route 
+# import prog_models.models.trajectory_generation.trajectory.trajectory as trajectory
+# from prog_models.models.trajectory_generation.vehicles import AircraftModels
+# from prog_models.models.trajectory_generation.vehicles.control import allocation_functions
+
+from prog_models.models.trajectory_generation.trajectory_generation import TrajGen 
 
 import numpy as np
 
 def run_example(): 
 
+    traj_gen = TrajGen()
+
+    x0_test = traj_gen.initialize()
+
+    a = 1
+
     # Flight info
     # ----------------------------------------------
-    flight_file    = 'src/prog_models/models/trajectory_generation/data/20181207_011200_Flight.txt'  # flight plan file txt
-    flight_name    = 'LaRC_20181207'                    # give it a name for saving
-    aircraft_name  = 'aircraft-1'                       # give it a name for saving
+    # flight_file    = 'src/prog_models/models/trajectory_generation/data/20181207_011200_Flight.txt'  # flight plan file txt
+    # flight_name    = 'LaRC_20181207'                    # give it a name for saving
+    # aircraft_name  = 'aircraft-1'                       # give it a name for saving
 
     # Simulation params
     # --------------
-    sim_params = dict(dt=0.2, # s, simulation dt
-                      gravity=9.81)       # m/s^2, gravity magnitude
+    # sim_params = dict(dt=0.2, # s, simulation dt
+    #                   gravity=9.81)       # m/s^2, gravity magnitude
 
-    traj_speed_params = dict(cruise=6.0,  # m/s, avg speed in-between way-points
-                             ascent=3.0,  # m/s, vertical speed (up)
-                             descent=3.0, # m/s, vertical speed (down)
-                             landing=1.5)  # m/s, landing speed when altitude < 10m
-    traj_add_time_params = dict(hovering=10.0,   # s, only if necessary
-                                takeoff= 60.0,  # s, only if necessary
-                                landing=60.0)    # s, only if necessary
-    traj_ref_params = dict(nurbs_basis_length=2000, nurbs_order=5)  # Nurbs algorithm (do not touch unless traj generation fails)
+    # traj_speed_params = dict(cruise=6.0,  # m/s, avg speed in-between way-points
+    #                          ascent=3.0,  # m/s, vertical speed (up)
+    #                          descent=3.0, # m/s, vertical speed (down)
+    #                          landing=1.5)  # m/s, landing speed when altitude < 10m
+    # traj_add_time_params = dict(hovering=10.0,   # s, only if necessary
+    #                             takeoff= 60.0,  # s, only if necessary
+    #                             landing=60.0)    # s, only if necessary
+    # traj_ref_params = dict(nurbs_basis_length=2000, nurbs_order=5)  # Nurbs algorithm (do not touch unless traj generation fails)
 
     # SELECT FLIGHT PLAN AND CREATE TRAJECTORY
     # =======================================
-    flightplan = trajectory.load.get_flightplan(fname=flight_file)
-    lat, lon, alt, tstamps = flightplan['lat'], flightplan['lon'], flightplan['alt'], flightplan['timestamp']
+    # flightplan = trajectory.load.get_flightplan(fname=flight_file)
+    # lat, lon, alt, tstamps = flightplan['lat'], flightplan['lon'], flightplan['alt'], flightplan['timestamp']
 
     # Generate route
-    route_ = route.build(name=flight_name, lat=lat, lon=lon, alt=alt, departure_time=tstamps[0],
-                         cruise_speed=traj_speed_params['cruise'], 
-                         ascent_speed=traj_speed_params['ascent'], 
-                         descent_speed=traj_speed_params['descent'], 
-                         landing_speed=traj_speed_params['landing'],
-                         hovering_time=traj_add_time_params['hovering'], 
-                         add_takeoff_time=traj_add_time_params['takeoff'], 
-                         add_landing_time=traj_add_time_params['landing'])
+    # route_ = route.build(name=flight_name, lat=lat, lon=lon, alt=alt, departure_time=tstamps[0],
+    #                      cruise_speed=traj_speed_params['cruise'], 
+    #                      ascent_speed=traj_speed_params['ascent'], 
+    #                      descent_speed=traj_speed_params['descent'], 
+    #                      landing_speed=traj_speed_params['landing'],
+    #                      hovering_time=traj_add_time_params['hovering'], 
+    #                      add_takeoff_time=traj_add_time_params['takeoff'], 
+    #                      add_landing_time=traj_add_time_params['landing'])
     # Generate trajectory
-    ref_traj = trajectory.Trajectory(name=flight_name, route=route_)
-    ref_traj.generate(dt=sim_params['dt'], 
-                      nurbs_order=traj_ref_params['nurbs_order'], 
-                      gravity=sim_params['gravity'], 
-                      nurbs_basis_length=traj_ref_params['nurbs_basis_length'])
+    # ref_traj = trajectory.Trajectory(name=flight_name, route=route_)
+    # ref_traj.generate(dt=sim_params['dt'], 
+    #                   nurbs_order=traj_ref_params['nurbs_order'], 
+    #                   gravity=sim_params['gravity'], 
+    #                   nurbs_basis_length=traj_ref_params['nurbs_basis_length'])
 
-    init_pos = np.concatenate((ref_traj.cartesian_pos[0,:], ref_traj.attitude[0,:], 
-                                ref_traj.velocity[0,:], ref_traj.angular_velocity[0,:]), axis=0)
+    # init_pos = np.concatenate((ref_traj.cartesian_pos[0,:], ref_traj.attitude[0,:], 
+    #                             ref_traj.velocity[0,:], ref_traj.angular_velocity[0,:]), axis=0)
 
     # Aircraft Model:
      # Variable vehicle_params contains only a subset of all the vehicle parameters, since some are already defined by default 
     # by selecting model = 'djis1000'
-    vehicle_params = dict(model = 'djis1000',       # select aircraft model (only DJIS1000 for now)
-                          payload=0.0,              # kg, payload mass
-                          integrator_fn='RK4')      # -, integrator
+    # vehicle_params = dict(model = 'djis1000',       # select aircraft model (only DJIS1000 for now)
+    #                       payload=0.0,              # kg, payload mass
+    #                       integrator_fn='RK4')      # -, integrator
 
-    aircraft1 = AircraftModels.build_model(init_pos, 
-                                           ref_traj.dt, 
-                                           name=aircraft_name, 
-                                           model=vehicle_params['model'], 
-                                           integrator_fn=vehicle_params['integrator_fn'],
-                                           payload=vehicle_params['payload'])
+    # aircraft1 = AircraftModels.build_model(init_pos, 
+    #                                        ref_traj.dt, 
+    #                                        name=aircraft_name, 
+    #                                        model=vehicle_params['model'], 
+    #                                        integrator_fn=vehicle_params['integrator_fn'],
+    #                                        payload=vehicle_params['payload'])
 
 
     debug = 1
--- src/prog_models/models/trajectory_generation/trajectory_generation.py
+++ src/prog_models/models/trajectory_generation/trajectory_generation.py
@@ -1,7 +1,12 @@
 # Copyright © 2022 United States Government as represented by the Administrator of the
 # National Aeronautics and Space Administration.  All Rights Reserved.
 
-from .. import PrognosticsModel
+# from .. import PrognosticsModel
+from prog_models.prognostics_model import PrognosticsModel
+import prog_models.models.trajectory_generation.trajectory.route as route 
+import prog_models.models.trajectory_generation.trajectory.trajectory as trajectory
+from prog_models.models.trajectory_generation.vehicles import AircraftModels
+from prog_models.models.trajectory_generation.vehicles.control import allocation_functions
 
 import numpy as np
 
@@ -22,13 +27,85 @@ class TrajGen(PrognosticsModel):
 
     """
     events = [] # fill in ['EOD']
-    inputs = []
-    states = []
-    outputs = []
+    inputs = ['u_x', 'u_y', 'u_z', 'u_phi', 'u_theta', 'u_psi', 'u_vx', 'u_vy', 'u_vz', 'u_p', 'u_q', 'u_r']
+    states = ['x', 'y', 'z', 'phi', 'theta', 'psi', 'vx', 'vy', 'vz', 'p', 'q', 'r']
+    outputs = ['x', 'y', 'z', 'phi', 'theta', 'psi', 'vx', 'vy', 'vz', 'p', 'q', 'r']
     is_vectorized = True
 
     default_parameters = {  # Set to defaults
+        # Flight information
+        'flight_file': 'src/prog_models/models/trajectory_generation/data/20181207_011200_Flight.txt', 
+        'flight_name': 'LaRC_20181207', 
+        'aircraft_name': 'aircraft-1', 
+
+        # Simulation parameters:
+        'dt': 0.2, 
+        'gravity': 9.81,
+        'cruise_speed': 6.0,
+        'ascent_speed': 3.0,
+        'descent_speed': 3.0, 
+        'landing_speed': 1.5,
+        'hovering_time': 10.0,
+        'takeoff_time': 60.0, 
+        'landing_time': 60.0, 
+        'nurbs_basis_length': 2000, 
+        'nurbs_order': 5, 
+
+        # Vehicle params:
+        'vehicle_model': 'djis1000',
+        'vehicle_payload': 0.0,
+        'vehicle_integrator_fn': 'RK4'
     }
+
+    def initialize(self, u=None, z=None): # initialize(self, u : dict, z = None):
+        
+        flightplan = trajectory.load.get_flightplan(fname=self.parameters['flight_file'])
+        lat, lon, alt, tstamps = flightplan['lat'], flightplan['lon'], flightplan['alt'], flightplan['timestamp']
+
+        # Generate route
+        route_ = route.build(name=self.parameters['flight_name'], lat=lat, lon=lon, alt=alt, departure_time=tstamps[0],
+                            cruise_speed=self.parameters['cruise_speed'], 
+                            ascent_speed=self.parameters['ascent_speed'], 
+                            descent_speed=self.parameters['descent_speed'], 
+                            landing_speed=self.parameters['landing_speed'],
+                            hovering_time=self.parameters['hovering_time'], 
+                            add_takeoff_time=self.parameters['takeoff_time'], 
+                            add_landing_time=self.parameters['landing_time'])
+        # Generate trajectory
+        ref_traj = trajectory.Trajectory(name=self.parameters['flight_name'], route=route_)
+        ref_traj.generate(dt=self.parameters['dt'], 
+                        nurbs_order=self.parameters['nurbs_order'], 
+                        gravity=self.parameters['gravity'], 
+                        nurbs_basis_length=self.parameters['nurbs_basis_length'])
+
+        self.ref_traj = ref_traj
+        # self.parameters['ref_traj'] = ref_traj
+
+        # Initialize vehicle 
+        init_pos = np.concatenate((ref_traj.cartesian_pos[0,:], ref_traj.attitude[0,:], 
+                                    ref_traj.velocity[0,:], ref_traj.angular_velocity[0,:]), axis=0)
+        aircraft1 = AircraftModels.build_model(init_pos, 
+                                               ref_traj.dt, 
+                                               name=self.parameters['aircraft_name'], 
+                                               model=self.parameters['vehicle_model'], 
+                                               integrator_fn=self.parameters['vehicle_integrator_fn'],
+                                               payload=self.parameters['vehicle_payload'])
+        self.vehicle_model = aircraft1 
+
+        return self.StateContainer({
+            'x': ref_traj.cartesian_pos[0,0],
+            'y': ref_traj.cartesian_pos[0,1],
+            'z': ref_traj.cartesian_pos[0,2],
+            'phi': ref_traj.attitude[0,0],
+            'theta': ref_traj.attitude[0,1],
+            'psi': ref_traj.attitude[0,2],
+            'vx': ref_traj.velocity[0,0],
+            'vy': ref_traj.velocity[0,1],
+            'vz': ref_traj.velocity[0,2],
+            'p': ref_traj.angular_velocity[0,0],
+            'q': ref_traj.angular_velocity[0,1],
+            'r': ref_traj.angular_velocity[0,2]
+            })
 
     def dx(self, x : dict, u : dict):
         pass 
