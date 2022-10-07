6a9e719a34676943cb5023a93a26083e87c5141b
Christopher Teubert <christopher.a.teubert@nasa.gov>
2022-10-07 14:03:00 -0700

    Fix copyright

--- examples/serialization.py
+++ examples/serialization.py
@@ -1,4 +1,4 @@
-# Copyright © 2022 United States Government as represented by the Administrator of the
+# Copyright © 2021 United States Government as represented by the Administrator of the
 # National Aeronautics and Space Administration.  All Rights Reserved.
 
 """
