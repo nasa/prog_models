9529eabe53e31d71946eb993c1119c4d7a4f67c4
Katy <kjjarvis@ucdavis.edu>
2022-10-11 14:58:58 -0700

    Edit copyright in test_serialization

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,4 +1,4 @@
-# Copyright © 2022 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
+# Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
 import unittest
 import numpy as np
