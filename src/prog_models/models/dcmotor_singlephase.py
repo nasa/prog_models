bc2603b6c4932dd45a6c645c1898c312d423f97c
Matteo Corbetta <35466777+matteocorbetta@users.noreply.github.com>
2022-10-17 11:44:23 -0700

    Update src/prog_models/models/dcmotor_singlephase.py
    Co-authored-by: Christopher Teubert <christopher.a.teubert@nasa.gov>

--- src/prog_models/models/dcmotor_singlephase.py
+++ src/prog_models/models/dcmotor_singlephase.py
@@ -1,4 +1,5 @@
-
+# Copyright © 2021 United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration.  All Rights Reserved.
 
 # from .. import prognostics_model
 from prog_models import PrognosticsModel
