eefe7b4cb4725750afad46c0ee17f47c2cd0b9b4
Christopher Teubert <christopher.a.teubert@nasa.gov>
2022-11-07 13:47:32 -0800

    Draft Ensemble

--- src/prog_models/__init__.py
+++ src/prog_models/__init__.py
@@ -1,7 +1,9 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the
 # National Aeronautics and Space Administration.  All Rights Reserved.
 
+# PrognosticsModel must be first, since the others build on this
 from .prognostics_model import PrognosticsModel
+from .ensemble_model import EnsembleModel
 from .linear_model import LinearModel
 from .exceptions import ProgModelException, ProgModelInputException, ProgModelTypeError
 
