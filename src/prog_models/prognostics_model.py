201186699c6b5f677f65554b551ab21fbfd28434
Christopher Teubert <christopher.a.teubert@nasa.gov>
2022-12-19 16:35:56 -0800

    config['x'] issue

--- src/prog_models/prognostics_model.py
+++ src/prog_models/prognostics_model.py
@@ -836,7 +836,7 @@ class PrognosticsModel(ABC):
             raise ProgModelInputException("'horizon' must be a number, was a {}".format(type(config['horizon'])))
         if config['horizon'] < 0:
             raise ProgModelInputException("'horizon' must be positive, was {}".format(config['horizon']))
-        if 'x' is not None and not all([state in config['x'] for state in self.states]):
+        if config['x'] is not None and not all([state in config['x'] for state in self.states]):
             raise ProgModelInputException("'x' must contain every state in model.states")
         if 'thresholds_met_eqn' in config and not callable(config['thresholds_met_eqn']):
             raise ProgModelInputException("'thresholds_met_eqn' must be callable (e.g., function or lambda)")
@@ -848,7 +848,7 @@ class PrognosticsModel(ABC):
         # Setup
         t = config['t0']
         u = future_loading_eqn(t)
-        if 'x' is not None:
+        if config['x'] is not None:
             x = deepcopy(config['x'])
         else:
             x = self.initialize(u, first_output)
