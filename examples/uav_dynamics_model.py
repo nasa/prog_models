cc5d2f52bef60590e34ae0106d211ebcad2014d5
Katy <kjjarvis@ucdavis.edu>
2023-01-03 16:03:24 -0800

    Improved traj gen example, added visualization function,

--- examples/uav_dynamics_model.py
+++ examples/uav_dynamics_model.py
@@ -1,63 +1,130 @@
+# Copyright © 2021 United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration.  All Rights Reserved.
 
+"""
+Example of generating a trajectory for a rotorcraft through a set of coarse waypoints 
+"""
 from prog_models.models.uav_model.uav_model import UAVGen 
 
 import numpy as np
 
 def run_example(): 
 
-    # Example uses waypoints from flight LaRC_20181207
-    flight_name = 'LaRC_20181207'
+    # Example 1: generate trajectory from waypoints and ETAs 
 
-    # Define coarse waypoints: 
+    # Define coarse waypoints: waypoints must be defined with a dictionary of numpy arrays or as columns in a text file 
+    # See documentation for specific information on inputting waypoints 
     waypoints = {}
     waypoints['lat_deg'] = np.array([37.09776, 37.09776, 37.09776, 37.09798, 37.09748, 37.09665, 37.09703, 37.09719, 37.09719, 37.09719, 37.09719, 37.09748, 37.09798, 37.09776, 37.09776])
     waypoints['lon_deg'] = np.array([-76.38631, -76.38629, -76.38629, -76.38589, -76.3848, -76.38569, -76.38658, -76.38628, -76.38628, -76.38628, -76.38628, -76.3848, -76.38589, -76.38629, -76.38629])
     waypoints['alt_ft'] = np.array([-1.9682394, 164.01995, 164.01995, 164.01995, 164.01995, 164.01995, 164.01995, 164.01995, 0.0, 0.0, 164.01995, 164.01995, 164.01995, 164.01995, 0.0])
-    # waypoints['time_unix'] = np.array([1544188336, 1544188358, 1544188360, 1544188377, 1544188394, 1544188411, 1544188428, 1544188496, 1544188539, 1544188584, 1544188601, 1544188635, 1544188652, 1544188672, 1544188692])
+    waypoints['time_unix'] = np.array([1544188336, 1544188358, 1544188360, 1544188377, 1544188394, 1544188411, 1544188428, 1544188496, 1544188539, 1544188584, 1544188601, 1544188635, 1544188652, 1544188672, 1544188692])
 
-    # Parameters:
-    params = {
-        'flight_plan': waypoints,
+    # Define model parameters:
+    params_1 = {
+        'flight_plan': waypoints, # Specify waypoints 
+        'dt': 0.1, # Define time step for model generation; this value is also used for simulate_to functionality 
+        'vehicle_model': 'tarot18', # Define vehicle
+    }
+
+    # Create a model object, define noise, and initialize
+    uav_1 = UAVGen(**params_1)
+    uav_1.parameters['process_noise'] = 0
+    x0 = uav_1.initialize()
+
+    # Define future loading function to return empty InputContainer, since there is no user-defined loading in trajectory generation 
+    def future_loading_1(t, x=None):
+        return uav_1.InputContainer({}) 
+
+    # Set simulation options 
+    options = {
+        # 'dt': 0.1, # Note that this value isn't used internally, as simulate_to dt is defined as params_1['dt]; if dt is defined here as any other value, a warning will be returned
+        'save_freq': params_1['dt'] 
+    }
+
+    # Generate trajectory
+    traj_results_1 = uav_1.simulate_to_threshold(future_loading_1, **options)
+
+    # Visualize results:
+    # Plot reference trajectory and generated trajectory - use 'visualize_traj' function in the UAVGen class
+    uav_1.visualize_traj(traj_results_1)
+    
+    # Plot internal states 
+    traj_results_1.outputs.plot(keys = ['x', 'y', 'z'], ylabel = 'Cartesian position',title='Example 1 Predicted Outputs')
+    traj_results_1.outputs.plot(keys = ['phi', 'theta', 'psi'], ylabel = 'Pitch, roll, and yaw',title='Example 1 Predicted Outputs')
+    traj_results_1.outputs.plot(keys = ['vx', 'vy', 'vz'], ylabel = 'Velocities',title='Example 1 Predicted Outputs')
+    traj_results_1.outputs.plot(keys = ['p', 'q', 'r'], ylabel = 'Angular velocities',title='Example 1 Predicted Outputs')
+
+
+    # Example 2: generate trajectory from waypoints and speeds 
+    # For this example, we define waypoints without ETAs, and instead add speed specifications 
+    
+    # Use same waypoints, but delete the ETAs
+    del waypoints['time_unix']
+
+    # Define model parameters - this time we must specify speeds since ETAs are not given as input
+    params_2 = {
+        'flight_plan': waypoints, # Specify waypoints 
         'dt': 0.1, 
-        'cruise_speed': 6.0,
+        'vehicle_model': 'tarot18',
+        'cruise_speed': 6.0, # Additionally specify speeds 
         'ascent_speed': 3.0,
         'descent_speed': 3.0, 
         'landing_speed': 1.5,
-        'vehicle_model': 'tarot18', # djis1000, tarot18
     }
 
-    # Generate UAV instance with waypoints defined in dictionary 
-    # traj_gen = UAVGen(flight_plan=waypoints)
+    # Create a model object, define noise, and initialize
+    uav_2 = UAVGen(**params_2)
+    uav_2.parameters['process_noise'] = 0
+    x0 = uav_2.initialize()
 
-    # Alternatively, waypoints can be specified in a table format and saved to a text file 
-    # flight_file = 'examples/uav_waypoints.txt'
-    # traj_gen = UAVGen(flight_file=flight_file)
+    # Define future loading function to return empty InputContainer, since there is no user-defined loading in trajectory generation 
+    def future_loading_2(t, x=None):
+        return uav_2.InputContainer({}) 
 
-    # Try with parameters:
-    traj_gen = UAVGen(**params)
-    
-    # No noise for testing 
-    traj_gen.parameters['process_noise'] = 0
-    traj_gen.parameters['measurement_noise'] = 0 
+    # Generate trajectory
+    traj_results_2 = uav_2.simulate_to_threshold(future_loading_2, **options)
 
-    x0_test = traj_gen.initialize()
+    # Visualize results:
+    # Plot reference trajectory and generated trajectory
+    uav_2.visualize_traj(traj_results_2)
 
-    def future_loading(t, x=None):
-        return traj_gen.InputContainer({})  # Loading defined internally 
 
-    options = {
-        'dt': 0.1,
-        # 'save_freq': 0.2
-        'integration_method': 'euler',
-        'print': True
+    # Example 3: using complex UAV model parameters 
+    # The UAV model class has other optional parameters for more specific modeling. Here, we illustrate the use of a few of these parameters.
+
+    # Define model parameters - this time we must specify speeds since ETAs are not given as input
+    params_3 = {
+        'flight_plan': waypoints, # Specify waypoints 
+        'dt': 0.1, 
+        'vehicle_model': 'tarot18',
+        'cruise_speed': 6.0, # Additionally specify speeds 
+        'ascent_speed': 3.0,
+        'descent_speed': 3.0, 
+        'landing_speed': 1.5,
+        'payload': 5.0, # kg, Add payload to vehicle
+        'hovering_time': 10.0, # s, Add hovering time between each waypoint
+        'final_time_buffer_sec': 15, # s, Defines acceptable time interval to reach final waypoint 
     }
 
-    # simulated_results = traj_gen.simulate_to(300,future_loading, **options)
-    simulated_results = traj_gen.simulate_to_threshold(future_loading, **options)
+    # Create a model object, define noise, and initialize
+    uav_3 = UAVGen(**params_3)
+    uav_3.parameters['process_noise'] = 0
+    x0 = uav_3.initialize()
 
-    debug = 1
+    # Define future loading function to return empty InputContainer, since there is no user-defined loading in trajectory generation 
+    def future_loading_3(t, x=None):
+        return uav_3.InputContainer({}) 
+
+    # Generate trajectory
+    traj_results_3 = uav_3.simulate_to_threshold(future_loading_3, **options)
+
+    # Visualize results:
+    # Plot reference trajectory and generated trajectory
+    uav_3.visualize_traj(traj_results_3)
 
 
 # This allows the module to be executed directly 
 if __name__ == '__main__':
     run_example()
+    
--- src/prog_models/models/uav_model/uav_model.py
+++ src/prog_models/models/uav_model/uav_model.py
@@ -9,6 +9,7 @@ from prog_models.models.uav_model.utilities import geometry
 
 import numpy as np
 import datetime
+from copy import deepcopy
 import prog_models.models.uav_model.utilities.geometry as geom
 from warnings import warn
 from prog_models.exceptions import ProgModelInputException
@@ -105,6 +106,7 @@ class UAVGen(PrognosticsModel):
         # Save final waypoint information for threshold_met and event_state 
         self.parameters['final_time'] = datetime.datetime.timestamp(route_.eta[-1]) - datetime.datetime.timestamp(route_.eta[0])
         coord_end = geometry.Coord(route_.lat[0], route_.lon[0], route_.alt[0])
+        self.coord_transform = deepcopy(coord_end)
         self.parameters['final_x'], self.parameters['final_y'], self.parameters['final_z'] = coord_end.geodetic2enu(route_.lat[-1], route_.lon[-1], route_.alt[-1])
         wypt_time_unix = [datetime.datetime.timestamp(route_.eta[iter]) - datetime.datetime.timestamp(route_.eta[0]) for iter in range(len(route_.eta))]
         wypt_x = []
@@ -339,20 +341,19 @@ class UAVGen(PrognosticsModel):
                 'TrajectoryComplete': True
             }
 
-    # def future_loading()
-        # User defined: m.simulate_to(10, m.future_loading)
-        # m.simulate_to(10,uav_model.controllers.xxx)
-
-        # ctrl = uav_model.controllers.ABc(m,....)
-        # m.simulate_to(10,ctrl)
-
-
     def simulate_to_threshold(self, future_loading_eqn, first_output = None, threshold_keys = None, **kwargs):
         
-        if kwargs['dt'] != self.parameters['dt']:
-            warn("dt must be equal to simulation dt value. To change the simulate_to time step, change the simulation parameter 'dt' value")
+        if 'dt' in kwargs and kwargs['dt'] != self.parameters['dt']:
+            if isinstance(kwargs['dt'], int): 
+                warn("dt value for simulation must be equal to the constant dt value defined in model generation. To change the simulate_to time step, change the model parameter 'dt' value")
+            else: 
+                warn("dt value for simulation must be equal to the constant dt value defined in model generation. Other time step functionalities (e.g. 'auto' or function definition) are not supported with trajectory generation.")
         kwargs['dt'] = self.parameters['dt']
-        kwargs['save_freq'] = self.parameters['dt']
+        if 'save_pts' in kwargs:
+            warn("'save_pts' functionality is not available with trajectory generation. A trajectory can only be generated for the same dt value as specified in the model generation.")
+            kwargs['save_pts'] = []
+        if 'save_freq' not in kwargs:
+            kwargs['save_freq'] = self.parameters['dt'] # if save_freq not specified, set at dt for best output 
 
         def future_loading_new(t, x=None): 
             if t == 0:
@@ -381,3 +382,68 @@ class UAVGen(PrognosticsModel):
         # Simulate to threshold 
         results = super().simulate_to_threshold(future_loading_new,first_output, threshold_keys, **kwargs)
         return results 
+
+    def visualize_traj(self, pred):
+
+        import matplotlib.pyplot as plt
+
+        # Conversions
+        # -----------
+        deg2rad = np.pi/180.0
+        rad2deg = 180.0/np.pi
+        
+        # Extract reference trajectory information
+        # ----------------------------------------
+        waypoints   = self.ref_traj.route
+        eta         = self.ref_traj.route.eta
+        depart_time = self.ref_traj.route.departure_time
+        time        = self.ref_traj.time
+        ref_pos     = self.ref_traj.geodetic_pos
+
+        # Extract predicted trajectory information
+        # ----------------------------------------
+        pred_time = pred.times
+        pred_x = [pred.outputs[iter]['x'] for iter in range(len(pred_time))]
+        pred_y = [pred.outputs[iter]['y'] for iter in range(len(pred_time))]
+        pred_z = [pred.outputs[iter]['z'] for iter in range(len(pred_time))]
+
+        pred_lat = []
+        pred_lon = []
+        pred_alt = []
+
+        for iter1 in range(len(pred_time)):
+            x_temp, y_temp, z_temp = self.coord_transform.enu2geodetic(pred_x[iter1], pred_y[iter1], pred_z[iter1])
+            pred_lat.append(x_temp)
+            pred_lon.append(y_temp)
+            pred_alt.append(z_temp)
+        
+        pred_lat_deg = [pred_lat[iter] * rad2deg for iter in range(len(pred_lat))]
+        pred_lon_deg = [pred_lon[iter] * rad2deg for iter in range(len(pred_lon))]
+
+        # Initialize Figure
+        # ----------------
+        params = dict(figsize=(13, 9), fontsize=14, linewidth=2.0, alpha_preds=0.6)
+        fig, (ax1, ax2) = plt.subplots(2)
+
+        # Plot trajectory predictions
+        # -------------------------
+        # First plot waypoints (dots) and reference trajectory (commanded, line)
+        ax1.plot(waypoints.lon * rad2deg, waypoints.lat  * rad2deg, 'o', color='tab:orange', alpha=0.5, markersize=10, label='__nolegend__')
+        ax1.plot(ref_pos[:, 1] * rad2deg, ref_pos[:, 0] * rad2deg, '--', linewidth=params['linewidth'], color='tab:orange', alpha=0.5, label='traj')
+        ax1.plot(pred_lon_deg, pred_lat_deg,'-', color='tab:blue', alpha=params['alpha_preds'], linewidth=params['linewidth'], label='prediction')
+
+        ax1.set_xlabel('longitude, deg', fontsize=params['fontsize'])
+        ax1.set_ylabel('latitude, deg', fontsize=params['fontsize'])
+        ax1.legend(fontsize=params['fontsize'])
+
+        # Add altitude plot
+        # -------------------------------------------------------
+        time_vector = [depart_time + datetime.timedelta(seconds=pred_time[ii]) for ii in range(len(pred_time))]
+        ax2.plot_date(eta, waypoints.alt, '--o', color='tab:orange', alpha=0.5, linewidth=params['linewidth'], label='__nolegend__')
+        ax2.plot(time_vector, pred_alt,'-', color='tab:blue',
+                      alpha=params['alpha_preds'], linewidth=params['linewidth'], label='__nolegend__')
+        
+        ax2.set_xlabel('time stamp, -', fontsize=params['fontsize'])
+        ax2.set_ylabel('altitude, m', fontsize=params['fontsize'])
+
+        return fig
