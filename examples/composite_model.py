ab25b3b181e6388cf2dce9a36619e324d102223f
Christopher Teubert <christopher.a.teubert@nasa.gov>
2023-01-06 17:20:41 -0800

    add to example

--- examples/composite_model.py
+++ examples/composite_model.py
@@ -1,16 +1,21 @@
-from prog_models.models import DCMotor, ESC
-from prog_models import PrognosticsModel
-from prog_models.composite_model import CompositeModel
-m_motor = DCMotor()
-m_esc = ESC()
+# Copyright © 2021 United States Government as represented by the Administrator of the
+# National Aeronautics and Space Administration.  All Rights Reserved.
 
+"""
+TODO(CT): DESCIRPTION
+"""
+
+from prog_models.models import DCMotor, ESC
+from prog_models import PrognosticsModel, CompositeModel
+
+# Load Model- used below
 def update_Cq(params):
     return {
         'C_q': params['c_q'] * params['rho'] * pow(params['D'], 5)
     }
 
 
-class Load(PrognosticsModel):
+class PropellerLoad(PrognosticsModel):
     inputs = ['v_rot']
     states = ['t_l']
     outputs = ['t_l']
@@ -38,24 +43,34 @@ class Load(PrognosticsModel):
     def output(self, x):
         return x
 
-m_load = Load()
+def run_example():
+    # First, lets define the composite models
+    m_motor = DCMotor()
+    m_esc = ESC()
+    m_load = PropellerLoad()
 
-m_composite = CompositeModel(
-    (m_esc, m_load, m_motor), 
-    connections = [
-        ('DCMotor.theta', 'ESC.theta'),
-        ('ESC.v_a', 'DCMotor.v_a'),
-        ('ESC.v_b', 'DCMotor.v_b'),
-        ('ESC.v_c', 'DCMotor.v_c'),
-        ('Load.t_l', 'DCMotor.t_l'),
-        ('DCMotor.v_rot', 'Load.v_rot')],
-    outputs = {'DCMotor.v_rot', 'DCMotor.theta'})
-print('\ninputs: ', m_composite.inputs)
-print('states: ', m_composite.states)
-print('outputs: ', m_composite.outputs)
-x0 = m_composite.initialize()
-print(x0)
-print(m_composite.next_state(x0, {'ESC.v': 10, 'ESC.duty': 0.5}, 0.1))
-print(m_composite.output(x0))
-print(m_composite.event_state(x0))
-print(m_composite.threshold_met(x0))
+    # Now let's combine them into a single composite model
+    # This model will then behave as a single model
+    m_composite = CompositeModel(
+        (m_esc, m_load, m_motor), 
+        connections = [
+            ('DCMotor.theta', 'ESC.theta'),
+            ('ESC.v_a', 'DCMotor.v_a'),
+            ('ESC.v_b', 'DCMotor.v_b'),
+            ('ESC.v_c', 'DCMotor.v_c'),
+            ('Load.t_l', 'DCMotor.t_l'),
+            ('DCMotor.v_rot', 'Load.v_rot')],
+        outputs = {'DCMotor.v_rot', 'DCMotor.theta'})
+    
+    print('inputs: ', m_composite.inputs)
+    print('states: ', m_composite.states)
+    print('outputs: ', m_composite.outputs)
+    x0 = m_composite.initialize()
+    print(x0)
+    print(m_composite.next_state(x0, {'ESC.v': 10, 'ESC.duty': 0.5}, 0.1))
+    print(m_composite.output(x0))
+    print(m_composite.event_state(x0))
+    print(m_composite.threshold_met(x0))
+
+if __name__ == '__main__':
+    run_example()
