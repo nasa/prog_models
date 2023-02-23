f361ee62d2dd6f7cf249373fa71a399f9098d51a
Aditya Tummala <adi.tummala@gmail.com>
2023-02-23 02:58:01 -0800

    =p

--- tests/test_base_models.py
+++ tests/test_base_models.py
@@ -13,9 +13,8 @@ sys.path.append(join(dirname(__file__), ".."))
 
 from prog_models import *
 from prog_models.models import *
-from prog_models.models.test_models.linear_models import (
-    OneInputNoOutputNoEventLM, OneInputOneOutputNoEventLM, OneInputNoOutputOneEventLM)
-from prog_models.models.thrown_object import LinearThrownObject, LinearThrownObject2, LinearThrownObjectDiffKey, LinearThrownObject3
+from prog_models.models.test_models.linear_models import (OneInputNoOutputNoEventLM, OneInputOneOutputNoEventLM, OneInputNoOutputOneEventLM, OneInputOneOutputNoEventLMPM)
+
 
 class MockModel():
     states = ['a', 'b', 'c', 't']
@@ -68,6 +67,42 @@ def derived_callback3(config):
     return {  # Testing 2nd chained update 
         'p4': -2 * config['p2'], 
     }
+
+class LinearThrownObject(LinearModel):
+    inputs = [] 
+    states = ['x', 'v']
+    outputs = ['x']
+    events = ['impact']
+
+    A = np.array([[0, 1], [0, 0]])
+    E = np.array([[0], [-9.81]])
+    C = np.array([[1, 0]])
+    F = None # Will override method
+
+    default_parameters = {
+        'thrower_height': 1.83,  # m
+        'throwing_speed': 40,  # m/s
+        'g': -9.81  # Acceleration due to gravity in m/s^2
+    }
+
+    def initialize(self, u=None, z=None):
+        return self.StateContainer({
+            'x': self.parameters['thrower_height'],  # Thrown, so initial altitude is height of thrower
+            'v': self.parameters['throwing_speed']  # Velocity at which the ball is thrown - this guy is a professional baseball pitcher
+            })
+    
+    def threshold_met(self, x):
+        return {
+            'falling': x['v'] < 0,
+            'impact': x['x'] <= 0
+        }
+
+    def event_state(self, x): 
+        x_max = x['x'] + np.square(x['v'])/(-self.parameters['g']*2) # Use speed and position to estimate maximum height
+        return {
+            'falling': np.maximum(x['v']/self.parameters['throwing_speed'],0),  # Throwing speed is max speed
+            'impact': np.maximum(x['x']/x_max,0) if x['v'] < 0 else 1  # 1 until falling begins, then it's fraction of height
+        }
 
 class MockModelWithDerived(MockProgModel):
     param_callbacks = {
@@ -143,39 +178,8 @@ class TestModels(unittest.TestCase):
         self.assertAlmostEqual(m.parameters['p3'], 5, 5)
         self.assertAlmostEqual(m.parameters['p4'], -10, 5)
 
-
-
-    #Move this to the bottom after done testing
-    def test_parameter_equality(self):
-        # Tests to include:
-            # A key exists in one and not in another, for both directions.
-            # Wrong parameters
-
-        m1 = LinearThrownObject()
-        m2 = LinearThrownObject()
-
-        self.assertTrue(m1.parameters == m2.parameters) #Checking too see if the parameters are equal
-        self.assertTrue(m2.parameters == m1.parameters) #Parameters should be equal
-
-        m3 = LinearThrownObject2() # A model with a different throwing speed
-
-        self.assertFalse(m1.parameters == m3.parameters)
-        self.assertFalse(m3.parameters == m1.parameters) # Checking both directions 
-
-        m4 = LinearThrownObjectDiffKey() # Model with an extra default parameter.
-
-        self.assertFalse(m1.parameters == m4.parameters)
-        self.assertFalse(m4.parameters == m1.parameters) # checking both directions
-
-        m5 = LinearThrownObject3() # Model with incorrectly initalized throwing height
-
-        self.assertFalse(m1.parameters == m5.parameters) 
-        self.assertFalse(m5.parameters == m1.parameters) 
-
-        self.assertTrue(m1.parameters == m2.parameters) # Checking too see previous equal statements stay the same
-        self.assertTrue(m2.parameters == m1.parameters) 
-
     def test_broken_models(self):
+
 
         class missing_states(PrognosticsModel):
             inputs = ['i1', 'i2']
@@ -188,6 +192,7 @@ class TestModels(unittest.TestCase):
             def output(self, x):
                 pass
         
+
         class empty_states(PrognosticsModel):
             states = []
             inputs = ['i1', 'i2']
@@ -1131,6 +1136,7 @@ class TestModels(unittest.TestCase):
     def test_composite(self):
         m1 = OneInputOneOutputNoEventLM()
         m2 = OneInputNoOutputOneEventLM()
+        m1_withpm = OneInputOneOutputNoEventLMPM()
 
         # Test with no connections
         m_composite = CompositeModel([m1, m1])
@@ -1138,7 +1144,8 @@ class TestModels(unittest.TestCase):
         self.assertSetEqual(m_composite.inputs, {'OneInputOneOutputNoEventLM.u1', 'OneInputOneOutputNoEventLM_2.u1'})
         self.assertSetEqual(m_composite.outputs, {'OneInputOneOutputNoEventLM.z1', 'OneInputOneOutputNoEventLM_2.z1'})
         self.assertSetEqual(m_composite.events, set())
- 
+        self.assertSetEqual(m_composite.performance_metric_keys, set(), "Shouldn't have any performance metrics")
+
         x0 = m_composite.initialize()
         self.assertSetEqual(set(x0.keys()), {'OneInputOneOutputNoEventLM_2.x1', 'OneInputOneOutputNoEventLM.x1'})
         self.assertEqual(x0['OneInputOneOutputNoEventLM_2.x1'], 0)
@@ -1153,6 +1160,21 @@ class TestModels(unittest.TestCase):
         self.assertSetEqual(set(z.keys()), {'OneInputOneOutputNoEventLM_2.z1', 'OneInputOneOutputNoEventLM.z1'})
         self.assertEqual(z['OneInputOneOutputNoEventLM_2.z1'], 0)
         self.assertEqual(z['OneInputOneOutputNoEventLM.z1'], 1)
+        pm = m_composite.performance_metrics(x)
+        self.assertSetEqual(set(pm.keys()), set())
+
+        # With Performance Metrics
+        # Everything else should behave the same, so we're only testing the performance metrics
+        m_composite = CompositeModel([m1_withpm, m1_withpm])
+        self.assertSetEqual(m_composite.performance_metric_keys, {'OneInputOneOutputNoEventLMPM_2.x1+1', 'OneInputOneOutputNoEventLMPM.x1+1'})
+
+        x0 = m_composite.initialize()
+        u = m_composite.InputContainer({'OneInputOneOutputNoEventLMPM.u1': 1, 'OneInputOneOutputNoEventLMPM_2.u1': 0})
+        x = m_composite.next_state(x0, u, 1)
+        pm = m_composite.performance_metrics(x)
+        self.assertSetEqual(set(pm.keys()), {'OneInputOneOutputNoEventLMPM_2.x1+1', 'OneInputOneOutputNoEventLMPM.x1+1'})
+        self.assertEqual(pm['OneInputOneOutputNoEventLMPM_2.x1+1'], 1)
+        self.assertEqual(pm['OneInputOneOutputNoEventLMPM.x1+1'], 2)
 
         # Test with connections - output, no event
         m_composite = CompositeModel([m1, m1], connections=[('OneInputOneOutputNoEventLM.z1', 'OneInputOneOutputNoEventLM_2.u1')])
@@ -1258,7 +1280,6 @@ class TestModels(unittest.TestCase):
         self.assertSetEqual(m_composite.inputs, {'m1.u1',})
         self.assertSetEqual(m_composite.outputs, {'m1.z1', })
         self.assertSetEqual(m_composite.events, {'m2.x1 == 10', })
-    
 
 # This allows the module to be executed directly
 def run_tests():
