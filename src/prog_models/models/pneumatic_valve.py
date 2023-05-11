30d71a259120ba93b88590afda2069e6a7437576
Miryam S <mstraut21@gmail.com>
2023-05-11 15:54:01 -0700

    Update src/prog_models/models/pneumatic_valve.py
    Co-authored-by: Christopher Teubert <christopher.a.teubert@nasa.gov>

--- src/prog_models/models/pneumatic_valve.py
+++ src/prog_models/models/pneumatic_valve.py
@@ -6,7 +6,6 @@ import numpy as np
 import warnings
 
 from prog_models import PrognosticsModel
-from prog_models.utils.containers import DictLikeMatrixWrapper
 
 
 def calc_x(x: float, forces: float, Ls: float, new_x: float) -> float:
