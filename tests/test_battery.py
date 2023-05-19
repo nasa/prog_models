4fdeaf1dd6ea50232b8de779857e5692d779f137
Aditya Tummala <adi.tummala@gmail.com>
2023-05-19 13:32:58 -0700

    Revert Tests

--- tests/test_battery.py
+++ tests/test_battery.py
@@ -80,18 +80,4 @@ def main():
         raise Exception("Failed test")
 
 if __name__ == '__main__':
-    import cProfile
-    cProfile.run('main()', "output.dat")
-
-    import pstats
-    from pstats import SortKey
-
-    with open("output_time.txt", 'w') as f:
-        p = pstats.Stats("output.dat", stream=f)
-        p.sort_stats("time").print_stats()
-
-    with open("output_calls.txt", 'w') as f:
-        p = pstats.Stats("output.dat", stream=f)
-        p.sort_stats("calls").print_stats()
-    
-    # main
+    main()
