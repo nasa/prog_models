2fdadf0172b5d89619ed4830892f00c21900fff4
Miryam S <mstraut21@gmail.com>
2023-05-31 12:46:39 -0700

    Update src/prog_models/sim_result.py
    Co-authored-by: Christopher Teubert <christopher.a.teubert@nasa.gov>

--- src/prog_models/sim_result.py
+++ src/prog_models/sim_result.py
@@ -41,7 +41,7 @@ class SimResult(UserList):
         created for deprecation warning. [] continues to be handled by parent
         """
         warn(
-            '[] will be deprecated after version 1.5 of ProgPy. Users may use pandas Dataframe \'iloc\'  to access '
+            '[] for access by row number will be deprecated after version 1.5 of ProgPy. After v1.5, [] will access by column (e.g., data['state1']), Users may use \'iloc\' to access by row number (e.g., data.iloc[10])'
             'data by element.',
             DeprecationWarning, stacklevel=2)
         return super().__getitem__(item)
